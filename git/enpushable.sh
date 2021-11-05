7c3f5d0f6826201ca66d16ab84d0751965219625
TaiseiIto <taisei1212@outlook.jp>
2021-11-05 20:12:48 +0900

    enpushable

---
+++ git/enpushable.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# This program gives the push access right.
+# Only the developer can execute it.
+# Put the private key "~/.ssh/github" before the execution.
+
+currentdir=$(pwd)
+cd $(dirname $0)
+echo -n "Your GitHub email:"
+read email
+echo -n "Your GitHub name:"
+read name
+git config --global user.email $email
+git config --global user.name $name
+git remote set-url origin git@github.com:TaiseiIto/THEOS.git
+chmod 600 ~/.ssh/github
+cd $currentdir
+
