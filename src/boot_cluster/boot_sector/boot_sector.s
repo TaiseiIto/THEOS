5cfb8d2942c72b643ae273e559ad562b5dbd4551
TaiseiIto <taisei1212@outlook.jp>
2021-11-07 22:34:45 +0900

    writing boot sector

--- src/boot_cluster/boot_sector/boot_sector.s
+++ src/boot_cluster/boot_sector/boot_sector.s
@@ -8,29 +8,34 @@
 # 0x0100 clusters per disk
 
 # The disk layout
-# cluster 0x01 : First boot cluster
+# cluster 0x00 : Boot cluster
 #  sector 0x00 : Boot sector
 #  sector 0x01 : FSInfo sector
 #  sector 0x06 : Backup boot sector
 #  sector 0x07 : Backup FSInfo sector
-# cluster 0x02 : First FAT
-# cluster 0x03 : Second FAT
-# cluster 0x04 : Root directory
+# cluster 0x01 : empty
+# cluster 0x02 : empty
+# cluster 0x03 : empty
+# cluster 0x04 : First FAT
+# cluster 0x05 : Second FAT
+# clister 0x06 : Data section
+# ...
+# clister 0xff : Data section
 
 	.set	bytes_per_sector,	0x0200
 	.set	sectors_per_cluster,	0x08
-	.set	boot_sectors,		0x0008
+	.set	boot_sectors,		0x0020
 	.set	fats,			0x02
 	.set	root_directory_entries,	0x0100
-	.set	sectors,		0x0800	# The drive size = 1 MiB = 0x800 sectors * 0x0200 bytes
+	.set	sectors,		0x00000800	# The drive size = 1 MiB = 0x800 sectors * 0x0200 bytes
 	.set	media_type,		0xf8
-	.set	sectors_per_fat,	0x0008
+	.set	sectors_per_fat,	0x00000008
 	.set	sectors_per_track,	0x0008
 	.set	heads,			0x0002
 	.set	hidden_sectors,		0x00000000
 	.set	flags,			0x0000
 	.set	fat_version,		0x0000
-	.set	root_directory_cluster,	0x00000004
+	.set	root_directory_cluster,	0x00000002
 	.set	fsinfo_sector,		0x0001
 	.set	backup_boot_sector,	0x0006
 	.set	drive_number,		0x80
@@ -48,10 +53,10 @@ legacy_bios_entry:
 	.byte	sectors_per_cluster
 	.word	boot_sectors
 	.byte	fats
-	.word	root_directory_entries
-	.word	sectors
+	.word	0x0000
+	.word	0x0000
 	.byte	media_type
-	.word	sectors_per_fat
+	.word	0x0000
 	.word	sectors_per_track
 	.word	heads
 	.long	hidden_sectors
