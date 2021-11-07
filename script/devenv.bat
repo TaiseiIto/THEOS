c0cf5f15cd78a51d9770a5993f0819bd3b2b1f31
TaiseiIto <taisei1212@outlook.jp>
2021-11-07 10:12:49 +0900

    writing devenv on windows

--- script/devenv.bat
+++ script/devenv.bat
@@ -4,6 +4,7 @@ REM Usage: ./devenv.sh <docker command> <image name> <image tag> <container name
 REM This program creates and logs in to docker image "theos-devenv" and container "theos-devenv".
 REM This program should be called from "../Makefile" with command "make devenv".
 
+REM CHECK ARGS
 SET ERROR=FALSE
 IF "%1" == "" SET ERROR=TRUE
 IF "%2" == "" SET ERROR=TRUE
@@ -14,12 +15,25 @@ ECHO There should be 4 arguments.
 EXIT /B -1
 )
 
+REM PICK ARGS
+SET DOCKER=%1
+SET IMAGE=%2
+SET TAG=%3
+SET CONTAINER=%4
+
+REM MOVE TO DIRECTORY WHERE THIS SCRIPT IS PUT
 SET CURRENTDIR=%~DP1
 CALL :DIRNAME %0
 CD %DIRNAME%
-CD
+
+FOR /F "USEBACKQ DELIMS=" %%I IN ('%DOCKER% images') DO (
+	ECHO %%I | FIND /I "%IMAGE%" > NUL
+	IF ERRORLEVEL 1 ECHO There is not the image
+)
+
 CD %CURRENTDIR%
 
+REM REPRODUCE UNIX DIRNAME COMMAND
 :DIRNAME
 SET DIRNAME=%~DP1
 EXIT /B
