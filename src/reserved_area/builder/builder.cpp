137e2a594a60695392f3eb99390a78c53533ecf3
TaiseiIto <taisei1212@outlook.jp>
2021-11-08 09:08:12 +0900

    argc check of reserved area builder

--- src/reserved_area/builder/builder.cpp
+++ src/reserved_area/builder/builder.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 
-int main(void)
+int main(int argc, char const * const * const argv)
 {
-	std::cout << "Hello, World!" << std::endl;
+	if(argc != 4)
+	{
+		std::cerr << "Usage : $ ./builder ../boot_sector.bin ../fsinfo_sector.bin ../reserved_area.bin" << std::endl;
+	}
 	return EXIT_SUCCESS;
 }
 
