e81324927c3b9703680ac8a00b152bfa7ecc9b4d
TaiseiIto <taisei1212@outlook.jp>
2021-11-08 20:40:32 +0900

    read bytes per sector

--- src/reserved_area/builder/boot_sector.cpp
+++ src/reserved_area/builder/boot_sector.cpp
@@ -10,12 +10,14 @@ BootSector::BootSector(unsigned char const * const data)
 	{
 		if(i < jump_instructions_offset + jump_instructions_size)jump_instructions[i - jump_instructions_offset] = data[i];
 		else if(i < oem_identifier_offset + oem_identifier_size)oem_identifier += data[i];
+		else if(i < bytes_per_sector_offset + bytes_per_sector_size)((unsigned char *)&bytes_per_sector)[i - bytes_per_sector_offset] = data[i];
 	}
 
 	std::cout << "jump instructions = ";
 	for(unsigned int i = 0; i < sizeof(jump_instructions) / sizeof(jump_instructions[0]); i++)std::cout << std::hex << std::setfill('0') << std::setw(2) << (unsigned int)jump_instructions[i] << " ";
 	std::cout << std::endl;
 
-	std::cout << "oem_identifier = \"" << oem_identifier << "\"" << std::endl;
+	std::cout << "oem identifier = \"" << oem_identifier << "\"" << std::endl;
+	std::cout << "bytes per sector = 0x" << std::hex << std::setfill('0') << std::setw(4) << bytes_per_sector << std::endl;
 }
 
--- src/reserved_area/builder/boot_sector.hpp
+++ src/reserved_area/builder/boot_sector.hpp
@@ -7,12 +7,18 @@ class BootSector
 {
 private:
 	static const unsigned int size = 0x200;
+
 	static const unsigned int jump_instructions_offset = 0;
 	static const unsigned int jump_instructions_size = 3;
 	unsigned char jump_instructions[3];
+
 	static const unsigned int oem_identifier_offset = jump_instructions_offset + jump_instructions_size;
 	static const unsigned int oem_identifier_size = 8;
 	std::string oem_identifier;
+
+	static const unsigned int bytes_per_sector_offset = oem_identifier_offset + oem_identifier_size;
+	static const unsigned int bytes_per_sector_size = 2;
+	unsigned short bytes_per_sector;
 public:
 	BootSector(unsigned char const * const data);
 };
