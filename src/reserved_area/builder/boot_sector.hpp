68ef5d8279060d228ce20f2977dd043922216371
TaiseiIto <taisei1212@outlook.jp>
2021-11-08 21:24:48 +0900

    writing reserved area builder

--- src/reserved_area/builder/boot_sector.hpp
+++ src/reserved_area/builder/boot_sector.hpp
@@ -8,29 +8,29 @@ class BootSector
 private:
 	static const unsigned int size = 0x200;
 
-	static const unsigned int jump_instructions_offset = 0;
-	static const unsigned int jump_instructions_size = 3;
 	unsigned char jump_instructions[3];
+	static const unsigned int jump_instructions_offset = 0;
+	static const unsigned int jump_instructions_size = sizeof(jump_instructions);
 
+	std::string oem_identifier;
 	static const unsigned int oem_identifier_offset = jump_instructions_offset + jump_instructions_size;
 	static const unsigned int oem_identifier_size = 8;
-	std::string oem_identifier;
 
-	static const unsigned int bytes_per_sector_offset = oem_identifier_offset + oem_identifier_size;
-	static const unsigned int bytes_per_sector_size = 2;
 	unsigned short bytes_per_sector;
+	static const unsigned int bytes_per_sector_offset = oem_identifier_offset + oem_identifier_size;
+	static const unsigned int bytes_per_sector_size = sizeof(bytes_per_sector);
 
-	static const unsigned int sectors_per_cluster_offset = bytes_per_sector_offset + bytes_per_sector_size;
-	static const unsigned int sectors_per_cluster_size = 1;
 	unsigned char sectors_per_cluster;
+	static const unsigned int sectors_per_cluster_offset = bytes_per_sector_offset + bytes_per_sector_size;
+	static const unsigned int sectors_per_cluster_size = sizeof(sectors_per_cluster);
 
-	static const unsigned int reserved_sectors_offset = sectors_per_cluster_offset + sectors_per_cluster_size;
-	static const unsigned int reserved_sectors_size = 2;
 	unsigned short reserved_sectors;
+	static const unsigned int reserved_sectors_offset = sectors_per_cluster_offset + sectors_per_cluster_size;
+	static const unsigned int reserved_sectors_size = sizeof(reserved_sectors);
 
-	static const unsigned int fats_offset = reserved_sectors_offset + reserved_sectors_size;
-	static const unsigned int fats_size = 1;
 	unsigned char fats;
+	static const unsigned int fats_offset = reserved_sectors_offset + reserved_sectors_size;
+	static const unsigned int fats_size = sizeof(fats);
 public:
 	BootSector(unsigned char const * const data);
 };
