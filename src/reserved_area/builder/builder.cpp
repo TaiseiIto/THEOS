1433aada47e58989ce569a57734605d771f6cac5
TaiseiIto <taisei1212@outlook.jp>
2021-11-08 17:09:49 +0900

    reading boot sector

--- src/reserved_area/builder/builder.cpp
+++ src/reserved_area/builder/builder.cpp
@@ -1,8 +1,12 @@
 #include <fstream>
+#include <iomanip>
 #include <iostream>
+
+void print_binary(unsigned char const * const data, unsigned int size);
 
 int main(int argc, char const * const * const argv)
 {
+	char boot_sector[0x100];
 	std::ifstream boot_sector_file;
 	std::ifstream fsinfo_sector_file;
 	if(argc != 4)
@@ -16,12 +20,26 @@ int main(int argc, char const * const * const argv)
 		std::cerr << "Can't open" << argv[1] << std::endl;
 		return EXIT_FAILURE;
 	}
+	boot_sector_file.read(boot_sector, sizeof(boot_sector));
+	print_binary((unsigned char *)boot_sector, sizeof(boot_sector));
+	boot_sector_file.close();
 	fsinfo_sector_file.open(argv[2], std::ifstream::binary | std::ifstream::in);
 	if(!fsinfo_sector_file.is_open())
 	{
 		std::cerr << "Can't open" << argv[2] << std::endl;
 		return EXIT_FAILURE;
 	}
+	fsinfo_sector_file.close();
 	return EXIT_SUCCESS;
+}
+
+void print_binary(unsigned char const * const data, unsigned int size)
+{
+	for(unsigned int i = 0; i < size; i++)
+	{
+		if(i % 0x10 == 0)std::cout << std::endl;
+		std::cout << std::hex << std::setfill('0') << std::setw(2) << (unsigned int)data[i] << " ";
+	}
+	std::cout << std::endl;
 }
 
