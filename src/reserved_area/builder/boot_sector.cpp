aeebf1637d85a0e3afa5ce427ee1e87c169876ed
TaiseiIto <taisei1212@outlook.jp>
2021-11-08 17:51:55 +0900

    writing reserved area builder

--- src/reserved_area/builder/boot_sector.cpp
+++ src/reserved_area/builder/boot_sector.cpp
@@ -1,8 +1,16 @@
+#include <cstdlib>
+#include <iomanip>
 #include <iostream>
 #include "boot_sector.hpp"
 
 BootSector::BootSector(unsigned char const * const data)
 {
-	std::cout << "Hello, World!" << std::endl;
+	for(unsigned int i = 0; i < 0x200; i++)
+	{
+		if(i < jump_instructions_offset + jump_instructions_size)jump_instructions[i - jump_instructions_offset] = data[i];
+	}
+	std::cout << "jump instructions = ";
+	for(unsigned int i = 0; i < sizeof(jump_instructions) / sizeof(jump_instructions[0]); i++)std::cout << std::hex << std::setfill('0') << std::setw(2) << (unsigned int)jump_instructions[i] << " ";
+	std::cout << std::endl;
 }
 
--- src/reserved_area/builder/boot_sector.hpp
+++ src/reserved_area/builder/boot_sector.hpp
@@ -1,11 +1,17 @@
 #ifndef _BOOT_SECTOR_HPP_
 #define _BOOT_SECTOR_HPP_
 
+
 class BootSector
 {
+private:
+	static const unsigned int jump_instructions_offset = 0;
+	static const unsigned int jump_instructions_size = 3;
+	unsigned char jump_instructions[3];
 public:
 	BootSector(unsigned char const * const data);
 };
+
 
 #endif
 
