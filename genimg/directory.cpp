be49148481e55b392047bbf6ba01f36770117922
TaiseiIto <taisei1212@outlook.jp>
2021-11-10 21:32:23 +0900

    add directory path

--- genimg/directory.cpp
+++ genimg/directory.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include "directory.hpp"
 
-Directory::Directory(std::string path)
+Directory::Directory(std::string path): input_path(path), output_path("/")
 {
 	std::filesystem::directory_iterator my_iterator = std::filesystem::directory_iterator(path);
 	for(std::filesystem::directory_iterator i = std::filesystem::begin(my_iterator); i != std::filesystem::end(my_iterator); i++)std::cout << i->path() << std::endl;
--- genimg/directory.hpp
+++ genimg/directory.hpp
@@ -5,6 +5,8 @@
 
 class Directory
 {
+	std::string input_path; // directory path on the source repository
+	std::string output_path; // directory path on the disk image
 public:
 	Directory(std::string path);
 };
