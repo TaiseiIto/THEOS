9767b214b9cd884820bbbf3f029cbf11b503ed7e
TaiseiIto <taisei1212@outlook.jp>
2023-01-04 14:09:17 +0000

    imager #5

--- imager/src/main.rs
+++ imager/src/main.rs
@@ -1,17 +1,20 @@
 use std::env;
+use std::path::Path;
 
 fn main() {
     let args: Args = match analyse_args(env::args()) {
         Ok(args) => args,
-        Err(message) => panic!("{}", message),
+        Err(msg) => panic!("{}", msg),
     };
-    println!("{:?}", args);
+    if let Err(msg) = imager(args) {
+        panic!("{}", msg);
+    }
 }
 
 #[derive(Debug)]
 struct Args {
     boot_sector: String,
-    source_directory: String,
+    src_dir: String,
     destination: String,
 }
 
@@ -19,24 +22,49 @@ fn analyse_args(mut args: env::Args) -> Result<Args, String> {
     let usage: String = String::from("Usage: $ ./imager /path/to/boot/sector /path/to/source/directory /path/to/destination");
     let _my_path: String = match args.next() {
         Some(my_path) => my_path,
-        None => return Err(format!("{}\n{}\n", "Program path is not found.", usage)),
+        None => return Err(format!("{}\n{}\n", "Program path is not specified.", usage)),
     };
     let boot_sector: String = match args.next() {
         Some(boot_sector) => boot_sector,
-        None => return Err(format!("{}\n{}\n", "Boot sector is not found.", usage)),
+        None => return Err(format!("{}\n{}\n", "Boot sector is not specified.", usage)),
     };
-    let source_directory: String = match args.next() {
-        Some(source_directory) => source_directory,
-        None => return Err(format!("{}\n{}\n", "Boot sector is not found.", usage)),
+    let src_dir: String = match args.next() {
+        Some(src_dir) => src_dir,
+        None => return Err(format!("{}\n{}\n", "Source directory is not specified.", usage)),
     };
     let destination: String = match args.next() {
         Some(destination) => destination,
-        None => return Err(format!("{}\n{}\n", "Boot sector is not found.", usage)),
+        None => return Err(format!("{}\n{}\n", "Boot sector is not specified.", usage)),
     };
     Ok(Args {
         boot_sector,
-        source_directory,
+        src_dir,
         destination,
     })
+}
+
+fn imager(args: Args) -> Result<(), String> {
+    let boot_sector = Path::new(&args.boot_sector);
+    let src_dir = Path::new(&args.src_dir);
+    let destination = Path::new(&args.destination);
+    if !boot_sector.exists() {
+        return Err(String::from(format!("{} doesn't exist.", boot_sector.display())));
+    }
+    if boot_sector.is_dir() {
+        return Err(String::from(format!("{} is directory.", boot_sector.display())));
+    }
+    if !src_dir.exists() {
+        return Err(String::from(format!("{} doesn't exist.", src_dir.display())));
+    }
+    if src_dir.is_file() {
+        return Err(String::from(format!("{} is file.", src_dir.display())));
+    }
+    if destination.exists() {
+        return Err(String::from(format!("{} exists already.", destination.display())));
+    }
+    println!("boot_sector = {}", boot_sector.display());
+    println!("src_dir = {}", src_dir.display());
+    println!("destination = {}", destination.display());
+    Ok(())
 }
 
