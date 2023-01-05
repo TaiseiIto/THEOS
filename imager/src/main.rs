e58f2ecce2e7241abf86cb1801c2ae7d48d6ec28
TaiseiIto <taisei1212@outlook.jp>
2023-01-05 07:37:34 +0000

    imager #5

--- imager/src/main.rs
+++ imager/src/main.rs
@@ -36,25 +36,93 @@ fn imager(args: Args) {
     println!("dst = {}", dst.display());
     let boot_sector: BootSector = read_boot_sector(&boot_sector);
     println!("boot_sector.jump_boot = {:x?}", boot_sector.jump_boot);
+    println!("boot_sector.boot_signature = {:#x}", boot_sector.boot_signature);
 }
 
-const SECTOR_SIZE: usize = 0x200;
-const JUMP_BOOT_SIZE: usize = 0x3;
-const FILE_SYSTEM_NAME_SIZE: usize = 0x8;
-const MUST_BE_ZERO_SIZE: usize = 0x35;
-
 #[derive(Debug)]
-#[repr(packed)]
 struct BootSector {
-    jump_boot: [u8; JUMP_BOOT_SIZE],
-    unread: [u8; SECTOR_SIZE - JUMP_BOOT_SIZE],
+    jump_boot: [u8; 0x3],
+    file_system_name: [u8; 0x8],
+    must_be_zero: [u8; 0x35],
+    partition_offset: u64,
+    volume_length: u64,
+    fat_offset: u32,
+    fat_length: u32,
+    cluster_heap_offset: u32,
+    cluster_count: u32,
+    first_cluster_of_root_directory: u32,
+    volume_serial_number: u32,
+    file_system_revision: u16,
+    volume_flags: u16,
+    bytes_per_sector_shift: u8,
+    sector_per_cluster_shift: u8,
+    number_of_fats: u8,
+    drive_select: u8,
+    percent_in_use: u8,
+    reserved: [u8; 0x7],
+    boot_code: [u8; 0x186],
+    boot_signature: u16,
+}
+
+#[repr(packed)]
+struct PackedBootSector {
+    jump_boot: [u8; 0x3],
+    file_system_name: [u8; 0x8],
+    must_be_zero: [u8; 0x35],
+    partition_offset: u64,
+    volume_length: u64,
+    fat_offset: u32,
+    fat_length: u32,
+    cluster_heap_offset: u32,
+    cluster_count: u32,
+    first_cluster_of_root_directory: u32,
+    volume_serial_number: u32,
+    file_system_revision: u16,
+    volume_flags: u16,
+    bytes_per_sector_shift: u8,
+    sector_per_cluster_shift: u8,
+    number_of_fats: u8,
+    drive_select: u8,
+    percent_in_use: u8,
+    reserved: [u8; 0x7],
+    boot_code: [u8; 0x186],
+    boot_signature: u16,
+}
+
+impl PackedBootSector {
+    fn unpack(self) -> BootSector {
+        BootSector {
+            jump_boot: self.jump_boot,
+            file_system_name: self.file_system_name,
+            must_be_zero: self.must_be_zero,
+            partition_offset: self.partition_offset,
+            volume_length: self.volume_length,
+            fat_offset: self.fat_offset,
+            fat_length: self.fat_length,
+            cluster_heap_offset: self.cluster_heap_offset,
+            cluster_count: self.cluster_count,
+            first_cluster_of_root_directory: self.first_cluster_of_root_directory,
+            volume_serial_number: self.volume_serial_number,
+            file_system_revision: self.file_system_revision,
+            volume_flags: self.volume_flags,
+            bytes_per_sector_shift: self.bytes_per_sector_shift,
+            sector_per_cluster_shift: self.sector_per_cluster_shift,
+            number_of_fats: self.number_of_fats,
+            drive_select: self.drive_select,
+            percent_in_use: self.percent_in_use,
+            reserved: self.reserved,
+            boot_code: self.boot_code,
+            boot_signature: self.boot_signature,
+        }
+    }
 }
 
 fn read_boot_sector(boot_sector: &path::Path) -> BootSector {
     let boot_sector: Vec<u8> = fs::read(boot_sector).expect(&format!("Failed to open {}", boot_sector.display()));
-    let boot_sector: [u8; SECTOR_SIZE] = boot_sector.try_into().expect(&format!("The length of boot sector must be {}.", SECTOR_SIZE));
-    unsafe {
-        mem::transmute::<[u8; SECTOR_SIZE], BootSector>(boot_sector)
-    }
+    let boot_sector: [u8; mem::size_of::<PackedBootSector>()] = boot_sector.try_into().expect(&format!("The length of boot sector must be {}.", mem::size_of::<PackedBootSector>()));
+    let boot_sector: PackedBootSector = unsafe {
+        mem::transmute::<[u8; mem::size_of::<PackedBootSector>()], PackedBootSector>(boot_sector)
+    };
+    boot_sector.unpack()
 }
 
