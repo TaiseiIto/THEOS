cab521c9f8e3942cc6ac57f157abc240c39819fc
TaiseiIto <taisei1212@outlook.jp>
2023-01-05 07:10:24 +0000

    imager #5

--- imager/src/main.rs
+++ imager/src/main.rs
@@ -36,12 +36,6 @@ fn imager(args: Args) {
     println!("dst = {}", dst.display());
     let boot_sector: BootSector = read_boot_sector(&boot_sector);
     println!("boot_sector.jump_boot = {:x?}", boot_sector.jump_boot);
-    println!("boot_sector.file_system_name = {:?}", boot_sector.file_system_name);
-    println!("boot_sector.must_be_zero = {:x?}", boot_sector.must_be_zero);
-    println!("boot_sector.partition_offset = {:#x}", boot_sector.partition_offset);
-    println!("boot_sector.volume_length = {:#x}", boot_sector.volume_length);
-    println!("boot_sector.fat_offset = {:#x}", boot_sector.fat_offset);
-    println!("boot_sector.fat_length = {:#x}", boot_sector.fat_length);
 }
 
 const SECTOR_SIZE: usize = 0x200;
@@ -50,43 +44,17 @@ const FILE_SYSTEM_NAME_SIZE: usize = 0x8;
 const MUST_BE_ZERO_SIZE: usize = 0x35;
 
 #[derive(Debug)]
+#[repr(packed)]
 struct BootSector {
     jump_boot: [u8; JUMP_BOOT_SIZE],
-    file_system_name: [char; FILE_SYSTEM_NAME_SIZE],
-    must_be_zero: [u8; MUST_BE_ZERO_SIZE],
-    partition_offset: u64,
-    volume_length: u64,
-    fat_offset: u32,
-    fat_length: u32,
+    unread: [u8; SECTOR_SIZE - JUMP_BOOT_SIZE],
 }
 
 fn read_boot_sector(boot_sector: &path::Path) -> BootSector {
     let boot_sector: Vec<u8> = fs::read(boot_sector).expect(&format!("Failed to open {}", boot_sector.display()));
     let boot_sector: [u8; SECTOR_SIZE] = boot_sector.try_into().expect(&format!("The length of boot sector must be {}.", SECTOR_SIZE));
-    let mut offset: usize = 0;
-    let jump_boot: [u8; JUMP_BOOT_SIZE] = boot_sector[offset..offset + JUMP_BOOT_SIZE].try_into().expect("Can't read JumpBoot.");
-    offset += JUMP_BOOT_SIZE;
-    let file_system_name: [u8; FILE_SYSTEM_NAME_SIZE] = boot_sector[offset..offset + FILE_SYSTEM_NAME_SIZE].try_into().expect("Can't read FileSystemName.");
-    let file_system_name: Vec<char> = file_system_name.iter().map(|c| char::from(*c)).collect();
-    let file_system_name: [char; FILE_SYSTEM_NAME_SIZE] = file_system_name.try_into().expect("Can't interpret FileSystemName as [char; FILE_SYSTEM_NAME_SIZE].");
-    offset += FILE_SYSTEM_NAME_SIZE;
-    let must_be_zero: [u8; MUST_BE_ZERO_SIZE] = boot_sector[offset..offset + MUST_BE_ZERO_SIZE].try_into().expect("Can't read MustBeZero.");
-    offset += MUST_BE_ZERO_SIZE;
-    let partition_offset: u64 = boot_sector[offset..offset + mem::size_of::<u64>()].iter().rev().fold(0, |acc, byte| (acc << 8) + *byte as u64);
-    offset += mem::size_of_val(&partition_offset);
-    let volume_length: u64 = boot_sector[offset..offset + mem::size_of::<u64>()].iter().rev().fold(0, |acc, byte| (acc << 8) + *byte as u64);
-    offset += mem::size_of_val(&volume_length);
-    let fat_offset: u32 = boot_sector[offset..offset + mem::size_of::<u32>()].iter().rev().fold(0, |acc, byte| (acc << 8) + *byte as u32);
-    offset += mem::size_of_val(&fat_offset);
-    let fat_length: u32 = boot_sector[offset..offset + mem::size_of::<u32>()].iter().rev().fold(0, |acc, byte| (acc << 8) + *byte as u32);
-    BootSector {
-        jump_boot,
-        file_system_name,
-        must_be_zero,
-        partition_offset,
-        volume_length,
-        fat_offset,
-        fat_length,
+    unsafe {
+        mem::transmute::<[u8; SECTOR_SIZE], BootSector>(boot_sector)
     }
 }
 
