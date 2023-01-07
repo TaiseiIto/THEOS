cf27c8b8f09221bb562f5c35bce72d437d6c7b03
TaiseiIto <taisei1212@outlook.jp>
2023-01-07 12:54:27 +0000

    imager #5

--- imager/src/exfat/upcase_table.rs
+++ imager/src/exfat/upcase_table.rs
@@ -13,7 +13,7 @@ pub struct UpcaseTable {
 impl UpcaseTable {
     pub fn new() -> Self {
         Self {
-            map: (0x0000..0x007f)
+            map: (0x0000..0xffff)
                 .filter_map(|n| std::char::from_u32(n))
                 .map(|c| (c, c.to_uppercase().collect()))
                 .collect(),
@@ -26,7 +26,7 @@ impl Sectors for UpcaseTable {
         let mut map: Vec<(u16, u16)> = self.map
             .iter()
             .map(|(c, u)| (*c as u16, u.as_bytes().to_vec()))
-            .filter(|(_, u)| 2 <= u.len())
+            .filter(|(_, u)| u.len() <= 2)
             .filter_map(|(c, u)| {
                 let mut i = u.iter();
                 match i.next() {
