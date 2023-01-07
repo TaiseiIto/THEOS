5dffff39ad2d4be584bcb44dd2f9d81e999465b1
TaiseiIto <taisei1212@outlook.jp>
2023-01-07 09:50:52 +0000

    imager #5

--- imager/src/exfat/upcase_table.rs
+++ imager/src/exfat/upcase_table.rs
@@ -1,19 +1,29 @@
 use std::collections::HashMap;
+use super::Sectors;
 
 pub struct UpcaseTable {
-    map: HashMap<char, char>,
+    map: HashMap<char, String>,
 }
 
 impl UpcaseTable {
     pub fn new() -> Self {
-        let chars: Vec<(u32, Vec<u8>)> = (0x0000..0xffff)
-            .flat_map(|n| std::char::from_u32(n))
-            .map(|c| (c, c.to_uppercase().collect::<String>()))
-            .map(|(c, u)| (c as u32, u.as_bytes().to_vec()))
-            .collect();
         Self {
-            map: HashMap::new(),
+            map: (0x0000..0xffff)
+                .flat_map(|n| std::char::from_u32(n))
+                .map(|c| (c, c.to_uppercase().collect()))
+                .collect(),
         }
+    }
+}
+
+impl Sectors for UpcaseTable {
+    fn to_bytes(&self) -> Vec<super::RawSector> {
+        let bytes: Vec<(u16, Vec<u8>)> = self.map
+            .iter()
+            .map(|(c, u)| (*c as u16, u.as_bytes().to_vec()))
+            .filter(|(_, u)| 2 < u.len())
+            .collect();
+        vec![]
     }
 }
 
