670113890871e5ca108fd08c19bbcda6425aa2fe
TaiseiIto <taisei1212@outlook.jp>
2023-01-07 09:33:50 +0000

    imager #5

--- imager/src/exfat/upcase_table.rs
+++ imager/src/exfat/upcase_table.rs
@@ -6,10 +6,10 @@ pub struct UpcaseTable {
 
 impl UpcaseTable {
     pub fn new() -> Self {
-        let chars: Vec<(char, Vec<u8>)> = (0x0000..0xffff)
+        let chars: Vec<(u32, Vec<u8>)> = (0x0000..0xffff)
             .flat_map(|n| std::char::from_u32(n))
             .map(|c| (c, c.to_uppercase().collect::<String>()))
-            .map(|(c, u)| (c, u.as_bytes().to_vec()))
+            .map(|(c, u)| (c as u32, u.as_bytes().to_vec()))
             .collect();
         Self {
             map: HashMap::new(),
