924d2441170620a30c4ea7d40258c3348c0c2abf
TaiseiIto <taisei1212@outlook.jp>
2023-01-07 10:09:22 +0000

    imager #5

--- imager/src/exfat/upcase_table.rs
+++ imager/src/exfat/upcase_table.rs
@@ -9,7 +9,7 @@ impl UpcaseTable {
     pub fn new() -> Self {
         Self {
             map: (0x0000..0xffff)
-                .flat_map(|n| std::char::from_u32(n))
+                .filter_map(|n| std::char::from_u32(n))
                 .map(|c| (c, c.to_uppercase().collect()))
                 .collect(),
         }
@@ -18,10 +18,20 @@ impl UpcaseTable {
 
 impl Sectors for UpcaseTable {
     fn to_bytes(&self) -> Vec<super::RawSector> {
-        let bytes: Vec<(u16, Vec<u8>)> = self.map
+        let bytes: Vec<(u16, u16)> = self.map
             .iter()
             .map(|(c, u)| (*c as u16, u.as_bytes().to_vec()))
             .filter(|(_, u)| 2 < u.len())
+            .filter_map(|(c, u)| {
+                let mut i = u.iter();
+                match i.next() {
+                    Some(lower_byte) => match i.next() {
+                        Some(upper_byte) => Some((c, ((*upper_byte as u16) << 8) + *lower_byte as u16)),
+                        None => Some((c, *lower_byte as u16)),
+                    },
+                    None => None,
+                }
+            })
             .collect();
         vec![]
     }
