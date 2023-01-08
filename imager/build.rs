5a7f5d8b08c07cd84aa065bf3ade7884d36979bb
TaiseiIto <taisei1212@outlook.jp>
2023-01-08 14:14:29 +0000

    refactoring

--- imager/build.rs
+++ imager/build.rs
@@ -3,7 +3,7 @@ use std::env;
 fn main() {
     let prj_dir: &str = "CARGO_MANIFEST_DIR";
     let prj_dir: String = env::var(prj_dir).expect(&format!("Can't get {}", prj_dir));
-    println!("cargo:rustc-link-search={}/target/", prj_dir);
+    println!("cargo:rustc-link-search={}/lib/", prj_dir);
     println!("cargo:rustc-link-lib=stat");
 }
 
