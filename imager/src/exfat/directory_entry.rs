63b34a95c7a9f6abfc6f4a3dbf61b4e40d584f24
TaiseiIto <taisei1212@outlook.jp>
2023-01-09 08:45:59 +0000

    Directory entry #5

--- imager/src/exfat/directory_entry.rs
+++ imager/src/exfat/directory_entry.rs
@@ -10,11 +10,33 @@ pub enum TypeCode {
     FileName,
 }
 
+impl fmt::Display for TypeCode {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::AllocationBitmap => write!(f, "AllocationBitmap"),
+            Self::UpcaseTable => write!(f, "UpcaseTable"),
+            Self::VolumeLabel => write!(f, "VolumeLabel"),
+            Self::FileDirectory => write!(f, "FileDirectory"),
+            Self::StreamExtension => write!(f, "StreamExtension"),
+            Self::FileName => write!(f, "FileName"),
+        }
+    }
+}
+
 #[derive(Debug)]
 pub struct EntryType {
     type_code: TypeCode,
     type_importance: bool,
     type_category: bool,
     in_use: bool,
+}
+
+impl fmt::Display for EntryType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "entry_type.type_code = {}\n", self.type_code)?;
+        write!(f, "entry_type.type_importance = {}\n", self.type_importance)?;
+        write!(f, "entry_type.type_category = {}\n", self.type_category)?;
+        write!(f, "entry_type.in_use = {}", self.in_use)
+    }
 }
 
