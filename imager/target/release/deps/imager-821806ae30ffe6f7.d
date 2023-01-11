07d2dc1edeceb7768b3b46dbaef5420c246377f6
TaiseiIto <taisei1212@outlook.jp>
2023-01-11 12:46:16 +0900

    reset imager #5

---
+++ imager/target/release/deps/imager-821806ae30ffe6f7.d
@@ -0,0 +1,5 @@
+/root/THEOS/imager/target/release/deps/imager-821806ae30ffe6f7: src/main.rs
+
+/root/THEOS/imager/target/release/deps/imager-821806ae30ffe6f7.d: src/main.rs
+
+src/main.rs:
---
+++ imager/target/release/imager.d
@@ -0,0 +1,1 @@
+/root/THEOS/imager/target/release/imager: /root/THEOS/imager/src/main.rs
