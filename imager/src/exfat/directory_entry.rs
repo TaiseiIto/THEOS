691cc782f324dac31e732a6273e804728472a66f
TaiseiIto <taisei1212@outlook.jp>
2023-01-12 14:44:09 +0900

    stream extension #5

--- imager/src/exfat/directory_entry.rs
+++ imager/src/exfat/directory_entry.rs
@@ -10,7 +10,9 @@ pub enum DirectoryEntry {
         create_time: time::Time,
         modified_time: time::Time,
         accessed_time: time::Time,
+        stream_extension: Box<DirectoryEntry>,
     },
+    StreamExtension,
 }
 
 impl DirectoryEntry {
@@ -19,12 +21,18 @@ impl DirectoryEntry {
         let create_time: time::Time = time::Time::get_changed_time(path);
         let modified_time: time::Time = time::Time::get_modified_time(path);
         let accessed_time: time::Time = time::Time::get_accessed_time(path);
+        let stream_extension = Box::new(Self::stream_extension());
         Self::File {
             file_attributes,
             create_time,
             modified_time,
             accessed_time,
+            stream_extension,
         }
+    }
+
+    fn stream_extension() -> Self {
+        Self::StreamExtension
     }
 
     fn entry_type(&self) -> EntryType {
@@ -41,10 +49,23 @@ struct EntryType {
 }
 
 impl EntryType {
-    pub fn file() -> Self {
-        let type_code = TypeCode::file();
+    fn file() -> Self {
+        let type_code = TypeCode::File;
         let type_importance = false;
         let type_category = false;
+        let in_use = true;
+        Self {
+            type_code,
+            type_importance,
+            type_category,
+            in_use,
+        }
+    }
+
+    fn stream_extension() -> Self {
+        let type_code = TypeCode::StreamExtension;
+        let type_importance = false;
+        let type_category = true;
         let in_use = true;
         Self {
             type_code,
@@ -58,11 +79,15 @@ impl EntryType {
 #[derive(Debug)]
 enum TypeCode {
     File,
+    StreamExtension,
 }
 
 impl TypeCode {
-    pub fn file() -> Self {
-        Self::File
+    pub fn to_byte(&self) -> u8 {
+        match self {
+            Self::File => 0x05,
+            Self::StreamExtension => 0x00,
+        }
     }
 }
 
