1efffd8a11ad715d779f0345ae62e85f7e9f8c68
TaiseiIto <taisei1212@outlook.jp>
2023-01-16 22:17:07 +0900

    Upcase table to bytes #5

--- imager/src/exfat/upcase_table.rs
+++ imager/src/exfat/upcase_table.rs
@@ -31,6 +31,35 @@ impl UpcaseTable {
         }
     }
 
+    pub fn to_bytes(&self) -> Vec<u8> {
+        let mut map: Vec<(u16, u16)> = self.map
+            .iter()
+            .filter(|(c, u)| c != u)
+            .map(|(c, u)| (*c, *u))
+            .collect();
+        map.sort_by(|(left, _), (right, _)| left.partial_cmp(&right).expect("Can't convert upcase table into bytes!"));
+        let (mut words, last_c): (Vec<u16>, u16) = map
+            .iter()
+            .fold((vec![], 0), |(words, last_c), (c, u)| {
+                let mut words: Vec<u16> = words;
+                if last_c + 1 < *c {
+                    words.push(0xffff);
+                    words.push(c - last_c);
+                }
+                words.push(*u);
+                (words, *c)
+            });
+        if last_c != 0xffff {
+            words.push(0xffff);
+            words.push(0 - last_c);
+        }
+        words
+            .iter()
+            .map(|w| vec![*w as u8, (*w >> 8) as u8])
+            .flatten()
+            .collect()
+    }
+
     pub fn to_upcase(&self, c: u16) -> u16 {
         match self.map.get(&c) {
             Some(upcase) => *upcase,
