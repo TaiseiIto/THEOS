c447d13ae861d79dc80e660fb37768f49c8e09d9
TaiseiIto <taisei1212@outlook.jp>
2023-01-19 12:57:02 +0900

    Mersenne twister #5

--- imager/src/rand.rs
+++ imager/src/rand.rs
@@ -20,5 +20,27 @@ impl Generator {
             queue,
         }
     }
+
+    pub fn generate_u32(&mut self) -> u32 {
+        let a:   u32 = 0x9908b0df;
+        let b:   u32 = 0x9d2c5680;
+        let c:   u32 = 0xefc60000;
+        let l:   u32 = 0x7fffffff;
+        let m:   usize = 387;
+        let sr0: u32 = 11;
+        let sl1: u32 = 7;
+        let sl2: u32 = 15;
+        let sr3: u32 = 18;
+        let u:   u32 = 0x80000000;
+        let x:   u32 = (self.queue[0] & u) | (self.queue[1] & l);
+        let y:   u32 = self.queue[m] ^ (x >> 1) ^ (a * (x & 1));
+        self.queue.pop_front();
+        self.queue.push_back(y);
+        let y: u32 = y ^ y >> sr0;
+        let y: u32 = y ^ y << sl1 & b;
+        let y: u32 = y ^ y << sl2 & c;
+        let y: u32 = y ^ y >> sr3;
+        y
+    }
 }
 
