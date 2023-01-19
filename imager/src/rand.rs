775b24810f5302e2ee32d679dfdce43579a71156
TaiseiIto <taisei1212@outlook.jp>
2023-01-19 13:32:39 +0900

    Mersenne twister #5

--- imager/src/rand.rs
+++ imager/src/rand.rs
@@ -9,7 +9,7 @@ pub struct Generator {
 
 impl Generator {
     pub fn new(seed: u32) -> Self {
-        let (queue, _): (VecDeque<u32>, u32) = (0..QUEUE_SIZE)
+        let (queue, _): (VecDeque<u32>, u32) = (1..=QUEUE_SIZE)
             .fold((VecDeque::<u32>::new(), seed), |(queue, next), i| {
                 let mut queue: VecDeque<u32> = queue;
                 queue.push_back(next);
