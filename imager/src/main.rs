12325756ab2c1c69a7b190c35084001a3e779d06
TaiseiIto <taisei1212@outlook.jp>
2023-01-21 23:23:14 +0900

    Read exfat #5

--- imager/src/main.rs
+++ imager/src/main.rs
@@ -17,17 +17,33 @@ use {
 };
 
 fn main() {
-    let args = Args::new(env::args());
-    let mut rand_generator = rand::Generator::new(time::Time::current_time().unix_timestamp() as u32);
-    let exfat = exfat::Exfat::new(args.boot_sector, args.source_directory, &mut rand_generator);
-    let exfat: Vec<u8> = exfat.to_bytes();
-    io::stdout().write_all(&exfat).expect("Can't write image to stdout.");
+    match Args::new(env::args()) {
+        Args::Read {
+            image,
+        } => {
+            println!("Read {}", image.display());
+        },
+        Args::Write {
+            boot_sector,
+            source_directory,
+        } => {
+            let mut rand_generator = rand::Generator::new(time::Time::current_time().unix_timestamp() as u32);
+            let exfat = exfat::Exfat::new(boot_sector, source_directory, &mut rand_generator);
+            let exfat: Vec<u8> = exfat.to_bytes();
+            io::stdout().write_all(&exfat).expect("Can't write image to stdout.");
+        },
+    }
 }
 
 #[derive(Debug)]
-struct Args {
-    boot_sector: path::PathBuf,
-    source_directory: path::PathBuf,
+enum Args {
+    Read {
+        image: path::PathBuf,
+    },
+    Write {
+        boot_sector: path::PathBuf,
+        source_directory: path::PathBuf,
+    },
 }
 
 impl Args {
@@ -43,10 +59,16 @@ impl Args {
             .collect();
         let boot_sector: Option<&String> = args.get("-b");
         let source_directory: Option<&String> = args.get("-s");
-        if let (Some(boot_sector), Some(source_directory)) = (boot_sector, source_directory) {
+        let image: Option<&String> = args.get("-i");
+        if let Some(image) = image {
+            let image = path::PathBuf::from(image);
+            Self::Read {
+                image,
+            }
+        } else if let (Some(boot_sector), Some(source_directory)) = (boot_sector, source_directory) {
             let boot_sector = path::PathBuf::from(boot_sector);
             let source_directory = path::PathBuf::from(source_directory);
-            Self {
+            Self::Write {
                 boot_sector,
                 source_directory,
             }
