67dd0f0b47d2957e487b8495c1be0054e19d6275
TaiseiIto <taisei1212@outlook.jp>
2023-02-05 16:00:30 +0900

    Determine file system #5

--- imager/src/file_system.rs
+++ imager/src/file_system.rs
@@ -2,11 +2,15 @@ pub mod exfat;
 
 use {
     std::{
+        fmt,
         fs,
         path::PathBuf,
         str,
     },
-    super::rand,
+    super::{
+        binary,
+        rand,
+    },
 };
 
 #[derive(Debug)]
@@ -21,11 +25,14 @@ impl FileSystem {
     pub fn new(boot_sector_candidates: Vec<PathBuf>, source_directory: PathBuf, rand_generator: &mut rand::Generator) -> Self {
         let file_system: Vec<FileSystemType> = boot_sector_candidates
             .iter()
-            .map(|boot_sector| FileSystemType::identify(&fs::read(&boot_sector).expect("Can't read file system.")))
+            .map(|boot_sector| FileSystemType::identify(&fs::read(&boot_sector).expect("Can't generate a file system.")))
             .collect();
         match file_system[0] {
             FileSystemType::Exfat => {
-                let boot_sector: PathBuf = boot_sector_candidates[0].clone();
+                let boot_sector: PathBuf = boot_sector_candidates
+                    .into_iter()
+                    .next()
+                    .expect("Can't generate a file system.");
                 let content = exfat::Exfat::new(boot_sector, source_directory, rand_generator);
                 Self::Exfat {
                     content,
@@ -34,6 +41,43 @@ impl FileSystem {
             FileSystemType::Fat12 |
             FileSystemType::Fat16 |
             FileSystemType::Fat32 => Self::Fat,
+        }
+    }
+
+    pub fn read(bytes: &Vec<u8>) -> Self {
+        let file_system = FileSystemType::identify(bytes);
+        match file_system {
+            FileSystemType::Exfat => {
+                let content = exfat::Exfat::read(bytes);
+                Self::Exfat {
+                    content,
+                }
+            },
+            FileSystemType::Fat12 |
+            FileSystemType::Fat16 |
+            FileSystemType::Fat32 => Self::Fat,
+        }
+    }
+}
+
+impl binary::Binary for FileSystem {
+    fn to_bytes(&self) -> Vec<u8> {
+        match self {
+            Self::Exfat {
+                content,
+            } => content.to_bytes(),
+            Self::Fat => vec![],
+        }
+    }
+}
+
+impl fmt::Display for FileSystem {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Exfat {
+                content,
+            } => write!(f, "{}", content),
+            Self::Fat => write!(f, ""),
         }
     }
 }
