35d4a19d23b5e005bdbb2d18f26507da41dcc309
TaiseiIto <taisei1212@outlook.jp>
2023-02-05 15:29:44 +0900

    Determine file system #5

--- imager/src/file_system.rs
+++ imager/src/file_system.rs
@@ -1,31 +1,53 @@
 pub mod exfat;
 
-use std::{
-    convert,
-    str,
+use {
+    std::{
+        path::PathBuf,
+        str,
+    },
+    super::rand,
 };
 
 #[derive(Debug)]
 pub enum FileSystem {
-    ExFat,
+    Exfat {
+        content: exfat::Exfat,
+    },
     Fat,
 }
 
-impl str::FromStr for FileSystem {
-    type Err = &'static str;
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        TryFrom::try_from(s)
+impl FileSystem {
+    pub fn new(boot_sector_candidates: Vec<PathBuf>, source_directory: PathBuf, rand_generator: &mut rand::Generator) {
     }
 }
 
-impl convert::TryFrom<&str> for FileSystem {
-    type Error = &'static str;
-    fn try_from(s: &str) -> Result<Self, Self::Error> {
-        let s: String = s.to_lowercase();
-        match &*s {
-            "exfat" => Ok(Self::ExFat),
-            "fat" => Ok(Self::Fat),
-            _ => Err("Can't interpret file system."),
+#[derive(Debug)]
+pub enum FileSystemType {
+    Exfat,
+    Fat12,
+    Fat16,
+    Fat32,
+}
+
+impl FileSystemType {
+    fn identify(bytes: &Vec<u8>) -> Self {
+        let file_system: &str = str::from_utf8(&bytes[3..11]).expect("Can't identify file sistem.");
+        match file_system {
+            "EXFAT   " => Self::Exfat,
+            _ => {
+                let file_system: &str = str::from_utf8(&bytes[54..62]).expect("Can't identify file system.");
+                match file_system {
+                    "FAT12   " => Self::Fat12,
+                    "FAT16   " => Self::Fat16,
+                    _ => {
+                        let file_system: &str = str::from_utf8(&bytes[82..90]).expect("Can't identify file system.");
+                        match file_system {
+                            "FAT32   " => Self::Fat32,
+                            _ => panic!("Can't identify file system."),
+                        }
+                    },
+                }
+            },
         }
     }
 }
