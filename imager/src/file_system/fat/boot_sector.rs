18ee27a89d847b34ebc9d2f129d0e9955ee8ba94
TaiseiIto <taisei1212@outlook.jp>
2023-02-05 21:22:59 +0900

    FAT boot sector #5

--- imager/src/file_system/fat/boot_sector.rs
+++ imager/src/file_system/fat/boot_sector.rs
@@ -2,3 +2,16 @@ mod fat12;
 mod fat16;
 mod fat32;
 
+#[derive(Debug)]
+pub enum BootSector {
+    Fat12 {
+        content: fat12::Fat12,
+    },
+    Fat16 {
+        content: fat16::Fat16,
+    },
+    Fat32 {
+        content: fat32::Fat32,
+    },
+}
+
