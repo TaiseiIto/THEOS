5ba2322ec8fc25f6571553ca144dc2980dd58e80
TaiseiIto <taisei1212@outlook.jp>
2023-02-05 21:43:12 +0900

    FAT boot sector #5

--- imager/src/file_system/fat/boot_sector.rs
+++ imager/src/file_system/fat/boot_sector.rs
@@ -2,6 +2,14 @@ mod fat12;
 mod fat16;
 mod fat32;
 
+use {
+    std::{
+        fs,
+        path::PathBuf,
+    },
+    super::super::file_system_type,
+};
+
 #[derive(Debug)]
 pub enum BootSector {
     Fat12 {
@@ -13,5 +21,33 @@ pub enum BootSector {
     Fat32 {
         content: fat32::Fat32,
     },
+}
+
+impl BootSector {
+    pub fn new(boot_sector: &PathBuf) -> Self {
+        let bytes: Vec<u8> = fs::read(boot_sector).expect("Can't generate a boot sector.");
+        let file_system_type = file_system_type::FileSystemType::identify(&bytes);
+        match file_system_type {
+            file_system_type::FileSystemType::Exfat => panic!("Can't generate a boot sector."),
+            file_system_type::FileSystemType::Fat12 => {
+                let content = fat12::Fat12::new(boot_sector);
+                Self::Fat12 {
+                    content,
+                }
+            },
+            file_system_type::FileSystemType::Fat16 => {
+                let content = fat16::Fat16::new(boot_sector);
+                Self::Fat16 {
+                    content,
+                }
+            },
+            file_system_type::FileSystemType::Fat32 => {
+                let content = fat32::Fat32::new(boot_sector);
+                Self::Fat32 {
+                    content,
+                }
+            },
+        }
+    }
 }
 
