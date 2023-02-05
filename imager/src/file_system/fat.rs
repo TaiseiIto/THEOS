21b943b7ed4c8738de2ee60685e24deaa7409c10
TaiseiIto <taisei1212@outlook.jp>
2023-02-05 18:39:19 +0900

    FAT12 boot sector #5

--- imager/src/file_system/fat.rs
+++ imager/src/file_system/fat.rs
@@ -1,35 +1,50 @@
 mod fat12;
 
 use {
-    std::fmt,
+    std::{
+        fmt,
+        path::PathBuf,
+    },
     super::super::binary::Binary,
 };
 
 #[derive(Debug)]
 pub struct Fat {
+    fat12_boot_sector: Option<fat12::boot_sector::BootSector>,
 }
 
 impl Fat {
-    pub fn new() -> Self {
+    pub fn new(fat12_boot_sector: PathBuf) -> Self {
+        let fat12_boot_sector: Option<fat12::boot_sector::BootSector> = Some(fat12::boot_sector::BootSector::new(fat12_boot_sector));
         Self {
+            fat12_boot_sector,
         }
     }
 
     pub fn read(bytes: &Vec<u8>) -> Self {
+        let fat12_boot_sector: Option<fat12::boot_sector::BootSector> = Some(fat12::boot_sector::BootSector::read(bytes));
         Self {
+            fat12_boot_sector,
         }
     }
 }
 
 impl Binary for Fat {
     fn to_bytes(&self) -> Vec<u8> {
-        vec![]
+        match self.fat12_boot_sector {
+            Some(fat12_boot_sector) => fat12_boot_sector.to_bytes(),
+            None => vec![],
+        }
     }
 }
 
 impl fmt::Display for Fat {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "")
+        let fat12_boot_sector: String = match self.fat12_boot_sector {
+            Some(fat12_boot_sector) => format!("{}", fat12_boot_sector),
+            None => "".to_string(),
+        };
+        write!(f, "{}", fat12_boot_sector)
     }
 }
 
