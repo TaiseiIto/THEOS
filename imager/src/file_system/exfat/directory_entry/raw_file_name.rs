a2472157046ee97f8618cba001e813712ac03e15
TaiseiIto <taisei1212@outlook.jp>
2023-02-08 20:39:41 +0900

    Implement Test trait #5

--- imager/src/file_system/exfat/directory_entry/raw_file_name.rs
+++ imager/src/file_system/exfat/directory_entry/raw_file_name.rs
@@ -1,11 +1,18 @@
 use {
-    std::mem,
+    std::{
+        convert::{
+            From,
+            Into,
+        },
+        mem,
+    },
     super::{
         DirectoryEntry,
         DIRECTORY_ENTRY_SIZE,
         FILE_NAME_BLOCK_LENGTH,
         Raw,
-    }
+        Test,
+    },
 };
 
 #[allow(dead_code)]
@@ -25,6 +32,47 @@ impl RawFileName {
     pub fn file_name(&self) -> [u16; FILE_NAME_BLOCK_LENGTH] {
         self.file_name
     }
+}
+
+impl From<&DirectoryEntry> for RawFileName {
+    fn from(directory_entry: &DirectoryEntry) -> Self {
+        let entry_type: u8 = directory_entry.entry_type().to_byte();
+        match directory_entry {
+            DirectoryEntry::FileName {
+                general_flags,
+                file_name,
+                next_file_name: _,
+            } => {
+                let general_flags: u8 = general_flags.into();
+                let file_name: [u16; FILE_NAME_BLOCK_LENGTH] = *file_name;
+                Self {
+                    entry_type,
+                    general_flags,
+                    file_name,
+                }
+            },
+            _ => panic!("Can't convert a DirectoryEntry into a RawFileName."),
+        }
+    }
+}
+
+impl From<&[u8; DIRECTORY_ENTRY_SIZE]> for RawFileName {
+    fn from(bytes: &[u8; DIRECTORY_ENTRY_SIZE]) -> Self {
+        unsafe {
+            mem::transmute::<[u8; DIRECTORY_ENTRY_SIZE], Self>(*bytes)
+        }
+    }
+}
+
+impl Into<[u8; DIRECTORY_ENTRY_SIZE]> for &RawFileName {
+    fn into(self) -> [u8; DIRECTORY_ENTRY_SIZE] {
+        unsafe {
+            mem::transmute::<RawFileName, [u8; DIRECTORY_ENTRY_SIZE]>(*self)
+        }
+    }
+}
+
+impl<'a> Test<'a> for RawFileName {
 }
 
 impl Raw for RawFileName {
