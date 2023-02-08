49a849be1c9b331ab341d98db7d49b517b734e58
TaiseiIto <taisei1212@outlook.jp>
2023-02-08 21:01:49 +0900

    Implement Test trait #5

--- imager/src/file_system/exfat/directory_entry/raw_volume_guid.rs
+++ imager/src/file_system/exfat/directory_entry/raw_volume_guid.rs
@@ -1,12 +1,16 @@
 use {
     std::{
-        convert::Into,
+        convert::{
+            From,
+            Into,
+        },
         mem,
     },
     super::{
         DirectoryEntry,
         DIRECTORY_ENTRY_SIZE,
         Raw,
+        Test,
     }
 };
 
@@ -30,6 +34,67 @@ impl RawVolumeGuid {
     pub fn volume_guid(&self) -> u128 {
         self.volume_guid
     }
+}
+
+impl From<&DirectoryEntry> for RawVolumeGuid {
+    fn from(directory_entry: &DirectoryEntry) -> Self {
+        let entry_type: u8 = directory_entry.entry_type().to_byte();
+        match directory_entry {
+            DirectoryEntry::VolumeGuid {
+                general_flags,
+                volume_guid,
+            } => {
+                let secondary_count: u8 = 0;
+                let set_checksum: u16 = 0;
+                let general_flags: u16 = Into::<u8>::into(general_flags) as u16;
+                let volume_guid: u128 = *volume_guid;
+                let reserved: [u8; 0xa] = [0; 0xa];
+                let raw_volume_guid = Self {
+                    entry_type,
+                    secondary_count,
+                    set_checksum,
+                    general_flags,
+                    volume_guid,
+                    reserved,
+                };
+                let bytes: Vec<u8> = raw_volume_guid.raw().to_vec();
+                let set_checksum: u16 = bytes
+                    .into_iter()
+                    .enumerate()
+                    .filter(|(i, _)| *i != 2 && *i != 3)
+                    .map(|(_, byte)| byte)
+                    .fold(0u16, |checksum, byte| (checksum << 15) + (checksum >> 1) + byte as u16);
+                Self {
+                    entry_type,
+                    secondary_count,
+                    set_checksum,
+                    general_flags,
+                    volume_guid,
+                    reserved,
+                }
+            },
+            _ => panic!("Can't convert a DirectoryEntry into a RawVolumeGuid."),
+        }
+    }
+}
+
+impl From<&[u8; DIRECTORY_ENTRY_SIZE]> for RawVolumeGuid {
+    fn from(bytes: &[u8; DIRECTORY_ENTRY_SIZE]) -> Self {
+        unsafe {
+            mem::transmute::<[u8; DIRECTORY_ENTRY_SIZE], Self>(*bytes)
+        }
+    }
+}
+
+impl Into<[u8; DIRECTORY_ENTRY_SIZE]> for &RawVolumeGuid {
+    fn into(self) -> [u8; DIRECTORY_ENTRY_SIZE] {
+        unsafe {
+            mem::transmute::<RawVolumeGuid, [u8; DIRECTORY_ENTRY_SIZE]>(*self)
+        }
+    }
+}
+
+impl<'a> Test<'a> for RawVolumeGuid {
 }
 
 impl Raw for RawVolumeGuid {
