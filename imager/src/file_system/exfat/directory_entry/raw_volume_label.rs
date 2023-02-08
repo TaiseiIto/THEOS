d343f2c68cb8fa33a2b6d361f8c88e2cdd2d92e1
TaiseiIto <taisei1212@outlook.jp>
2023-02-08 21:05:58 +0900

    Implement Test trait #5

--- imager/src/file_system/exfat/directory_entry/raw_volume_label.rs
+++ imager/src/file_system/exfat/directory_entry/raw_volume_label.rs
@@ -1,10 +1,17 @@
 use {
-    std::mem,
+    std::{
+        convert::{
+            From,
+            Into,
+        },
+        mem,
+    },
     super::{
         DirectoryEntry,
         DIRECTORY_ENTRY_SIZE,
         Raw,
-    }
+        Test,
+    },
 };
 
 pub const VOLUME_LABEL_MAX_LENGTH: usize = 11;
@@ -27,6 +34,56 @@ impl RawVolumeLabel {
     pub fn volume_label(&self) -> [u16; VOLUME_LABEL_MAX_LENGTH] {
         self.volume_label
     }
+}
+
+impl From<&DirectoryEntry> for RawVolumeLabel {
+    fn from(directory_entry: &DirectoryEntry) -> Self {
+        let entry_type: u8 = directory_entry.entry_type().to_byte();
+        match directory_entry {
+            DirectoryEntry::VolumeLabel {
+                volume_label,
+            } => {
+                let mut volume_label: Vec<u16> = volume_label
+                    .encode_utf16()
+                    .collect();
+                let character_count = volume_label.len() as u8;
+                while volume_label.len() < VOLUME_LABEL_MAX_LENGTH {
+                    volume_label.push(0x0000);
+                }
+                let (volume_label, _): (&[u16], &[u16]) = volume_label.split_at(VOLUME_LABEL_MAX_LENGTH);
+                let volume_label: [u16; VOLUME_LABEL_MAX_LENGTH] = volume_label
+                    .try_into()
+                    .expect("Can't convert volume label into [u16; VOLUME_LABEL_MAX_LENGTH].");
+                let reserved: u64 = 0;
+                Self {
+                    entry_type,
+                    character_count,
+                    volume_label,
+                    reserved,
+                }
+            },
+            _ => panic!("Can't convert a DirectoryEntry into a RawVolumeLabel."),
+        }
+    }
+}
+
+impl From<&[u8; DIRECTORY_ENTRY_SIZE]> for RawVolumeLabel {
+    fn from(bytes: &[u8; DIRECTORY_ENTRY_SIZE]) -> Self {
+        unsafe {
+            mem::transmute::<[u8; DIRECTORY_ENTRY_SIZE], Self>(*bytes)
+        }
+    }
+}
+
+impl Into<[u8; DIRECTORY_ENTRY_SIZE]> for &RawVolumeLabel {
+    fn into(self) -> [u8; DIRECTORY_ENTRY_SIZE] {
+        unsafe {
+            mem::transmute::<RawVolumeLabel, [u8; DIRECTORY_ENTRY_SIZE]>(*self)
+        }
+    }
+}
+
+impl<'a> Test<'a> for RawVolumeLabel {
 }
 
 impl Raw for RawVolumeLabel {
