c07aefc0779d8b38d5dffa6ee335c21ad4dfc44c
TaiseiIto <taisei1212@outlook.jp>
2023-02-09 17:36:12 +0900

    FAT long file name directory entry #5

--- imager/src/file_system/fat/directory_entry.rs
+++ imager/src/file_system/fat/directory_entry.rs
@@ -18,7 +18,41 @@ pub enum DirectoryEntry {
         written_time: time::Time,
         first_cluster: u32,
         size: usize,
+        long_file_name: Option<Box<Self>>,
     },
+    LongFileName {
+        name: [u16; LONG_FILE_NAME_LENGTH],
+        order: usize,
+        next: Option<Box<Self>>,
+    },
+}
+
+const LONG_FILE_NAME_LENGTH: usize = 13;
+
+impl DirectoryEntry {
+    fn long_file_name(mut name: Vec<u16>, order: usize) -> Self {
+        let (name, next): ([u16; LONG_FILE_NAME_LENGTH], Option<Box<Self>>) = if LONG_FILE_NAME_LENGTH <= name.len() {
+            let (name, next): (&[u16], &[u16]) = name.split_at(LONG_FILE_NAME_LENGTH);
+            let name: [u16; LONG_FILE_NAME_LENGTH] = name
+                .try_into()
+                .expect("Can't generate a long file name directory entry.");
+            let next: Vec<u16> = next.to_vec();
+            let next: Option<Box<Self>> = Some(Box::new(Self::long_file_name(next, order + 1)));
+            (name, next)
+        } else {
+            name.resize(LONG_FILE_NAME_LENGTH, 0x0000);
+            let name: [u16; LONG_FILE_NAME_LENGTH] = name
+                .try_into()
+                .expect("Can't generate a long file name directory entry.");
+            let next: Option<Box<Self>> = None;
+            (name, next)
+        };
+        Self::LongFileName {
+            name,
+            order,
+            next,
+        }
+    }
 }
 
 impl From<&node::Node> for DirectoryEntry {
@@ -34,6 +68,8 @@ impl From<&node::Node> for DirectoryEntry {
         } else {
             node.size()
         };
+        let long_file_name: Vec<u16> = name.encode_utf16().collect();
+        let long_file_name: Option<Box<Self>> = Some(Box::new(Self::long_file_name(long_file_name, 1)));
         Self::ShortFileName {
             name,
             attribute,
@@ -42,6 +78,7 @@ impl From<&node::Node> for DirectoryEntry {
             written_time,
             first_cluster,
             size,
+            long_file_name,
         }
     }
 }
