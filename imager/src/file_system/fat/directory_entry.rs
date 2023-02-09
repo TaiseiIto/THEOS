a3905e3d6e0752cc51996b1510a06f3d87abcb86
TaiseiIto <taisei1212@outlook.jp>
2023-02-09 15:04:09 +0900

    FAT directory entry #5

--- imager/src/file_system/fat/directory_entry.rs
+++ imager/src/file_system/fat/directory_entry.rs
@@ -1,2 +1,23 @@
 mod attribute;
 
+use {
+    std::convert::From,
+    super::node,
+};
+
+#[derive(Debug)]
+pub enum DirectoryEntry {
+    ShortFileName {
+        attribute: attribute::Attribute,
+    },
+}
+
+impl From<&node::Node> for DirectoryEntry {
+    fn from(node: &node::Node) -> Self {
+        let attribute = attribute::Attribute::from(node);
+        Self::ShortFileName {
+            attribute,
+        }
+    }
+}
+
