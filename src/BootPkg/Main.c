9510bf98101b9e7a507c3fa0d4fb3b9b3d76de4e
TaiseiIto <taisei1212@outlook.jp>
2023-02-26 21:35:45 +0900

    Rust loader #8

--- src/BootPkg/Main.c
+++
@@ -1,9 +0,0 @@
-#include <Uefi.h>
-#include <Library/UefiLib.h>
-
-EFI_STATUS EFIAPI Main(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE *system_table) {
-	Print(L"Hello, World!\n");
-	while(1);
-	return EFI_SUCCESS;
-}
-
