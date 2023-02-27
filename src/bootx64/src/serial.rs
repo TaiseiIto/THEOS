c8654d1f6800db731a5697b3b7097e6b0d79fcb3
TaiseiIto <taisei1212@outlook.jp>
2023-02-27 22:10:37 +0900

    Put char on serial #8

--- src/bootx64/src/serial.rs
+++ src/bootx64/src/serial.rs
@@ -1,3 +1,6 @@
+// References
+// https://wiki.osdev.org/Serial_Ports
+
 use super::asm;
 
 pub struct Serial {
@@ -10,7 +13,18 @@ pub const COM1: Serial = Serial {
 
 impl Serial {
     pub fn put_char(&self, byte: u8) {
+        while !self.can_send() {}
         asm::outb(self.port, byte);
+    }
+
+    fn can_send(&self) -> bool {
+        let line_status_register: asm::Port = self.line_status_register();
+        let line_status: u8 = asm::inb(line_status_register);
+        line_status & 0x20 != 0
+    }
+
+    fn line_status_register(&self) -> asm::Port {
+        self.port + 5
     }
 }
 
