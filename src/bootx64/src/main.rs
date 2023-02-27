c71945920885c2a4c022c070118a3d6961ba50a0
TaiseiIto <taisei1212@outlook.jp>
2023-02-27 19:50:48 +0900

    hlt #8

--- src/bootx64/src/main.rs
+++ src/bootx64/src/main.rs
@@ -3,27 +3,21 @@
 #![feature(abi_efiapi)]
 #![allow(stable_features)]
 
-use core::{
-    arch::asm,
-    panic::PanicInfo,
-};
+mod asm;
+
+use core::panic::PanicInfo;
 
 #[no_mangle]
 fn efi_main(_image_handle: u64, _system_table: u64) -> u64 {
-    // Infinite loop.
     loop {
-        unsafe {
-            asm!("hlt");
-        }
+        asm::hlt();
     }
 }
 
 #[panic_handler]
 fn panic(_panic: &PanicInfo) -> ! {
     loop {
-        unsafe {
-            asm!("hlt");
-        }
+        asm::hlt();
     }
 }
 
