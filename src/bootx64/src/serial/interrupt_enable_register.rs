d84c374cb97552218e6bc84428838de92dddd097
TaiseiIto <taisei1212@outlook.jp>
2023-03-04 18:34:32 +0900

    Interrupt enable register #8

--- src/bootx64/src/serial/interrupt_enable_register.rs
+++ src/bootx64/src/serial/interrupt_enable_register.rs
@@ -1,42 +1,57 @@
+// References
+// https://www.lookrs232.com/rs232/ier.htm
+
 use super::super::asm;
 
 pub struct InterruptEnableRegister {
-    data_available: bool,
-    transmitter_empty: bool,
-    line_status: bool,
+    received_data_available: bool,
+    transmitter_holding_register_empty: bool,
+    receiver_line_status: bool,
     modem_status: bool,
+    sleep: bool,
+    low_power: bool,
 }
 
-const DATA_AVAILABLE: u8 = 0x01;
-const TRANSMITTER_EMPTY: u8 = 0x02;
-const LINE_STATUS: u8 = 0x04;
+const RECEIVED_DATA_AVAILABLE: u8 = 0x01;
+const TRANSMITTER_HOLDING_REGISTER_EMPTY: u8 = 0x02;
+const RECEIVER_LINE_STATUS: u8 = 0x04;
 const MODEM_STATUS: u8 = 0x08;
+const SLEEP: u8 = 0x10;
+const LOW_POWER: u8 = 0x20;
 
 impl InterruptEnableRegister {
     pub fn disable_all_interrupts() -> Self {
-        let data_available: bool = false;
-        let transmitter_empty: bool = false;
-        let line_status: bool = false;
+        let received_data_available: bool = false;
+        let transmitter_holding_register_empty: bool = false;
+        let receiver_line_status: bool = false;
         let modem_status: bool = false;
+        let sleep: bool = false;
+        let low_power: bool = false;
         Self::new(
-            data_available,
-            transmitter_empty,
-            line_status,
+            received_data_available,
+            transmitter_holding_register_empty,
+            receiver_line_status,
             modem_status,
+            sleep,
+            low_power,
         )
     }
 
     pub fn new(
-        data_available: bool,
-        transmitter_empty: bool,
-        line_status: bool,
+        received_data_available: bool,
+        transmitter_holding_register_empty: bool,
+        receiver_line_status: bool,
         modem_status: bool,
+        sleep: bool,
+        low_power: bool,
     ) -> Self {
         Self {
-            data_available,
-            transmitter_empty,
-            line_status,
+            received_data_available,
+            transmitter_holding_register_empty,
+            receiver_line_status,
             modem_status,
+            sleep,
+            low_power,
         }
     }
 }
@@ -50,41 +65,55 @@ impl From<asm::Port> for InterruptEnableRegister {
 
 impl From<u8> for InterruptEnableRegister {
     fn from(byte: u8) -> Self {
-        let data_available: bool = byte & DATA_AVAILABLE != 0;
-        let transmitter_empty: bool = byte & TRANSMITTER_EMPTY != 0;
-        let line_status: bool = byte & LINE_STATUS != 0;
+        let received_data_available: bool = byte & RECEIVED_DATA_AVAILABLE != 0;
+        let transmitter_holding_register_empty: bool = byte & TRANSMITTER_HOLDING_REGISTER_EMPTY != 0;
+        let receiver_line_status: bool = byte & RECEIVER_LINE_STATUS != 0;
         let modem_status: bool = byte & MODEM_STATUS != 0;
+        let sleep: bool = byte & SLEEP != 0;
+        let low_power: bool = byte & LOW_POWER != 0;
         Self {
-            data_available,
-            transmitter_empty,
-            line_status,
+            received_data_available,
+            transmitter_holding_register_empty,
+            receiver_line_status,
             modem_status,
+            sleep,
+            low_power,
         }
     }
 }
 
 impl Into<u8> for &InterruptEnableRegister {
     fn into(self) -> u8 {
-        let data_available: u8 = match self.data_available {
-            true => DATA_AVAILABLE,
+        let received_data_available: u8 = match self.received_data_available {
+            true => RECEIVED_DATA_AVAILABLE,
             false => 0x00,
         };
-        let transmitter_empty: u8 = match self.transmitter_empty {
-            true => TRANSMITTER_EMPTY,
+        let transmitter_holding_register_empty: u8 = match self.transmitter_holding_register_empty {
+            true => TRANSMITTER_HOLDING_REGISTER_EMPTY,
             false => 0x00,
         };
-        let line_status: u8 = match self.line_status {
-            true => LINE_STATUS,
+        let receiver_line_status: u8 = match self.receiver_line_status {
+            true => RECEIVER_LINE_STATUS,
             false => 0x00,
         };
         let modem_status: u8 = match self.modem_status {
             true => MODEM_STATUS,
             false => 0x00,
         };
-        data_available
-        | transmitter_empty
-        | line_status
+        let sleep: u8 = match self.sleep {
+            true => SLEEP,
+            false => 0x00,
+        };
+        let low_power: u8 = match self.low_power {
+            true => LOW_POWER,
+            false => 0x00,
+        };
+        received_data_available
+        | transmitter_holding_register_empty
+        | receiver_line_status
         | modem_status
+        | sleep
+        | low_power
     }
 }
 
