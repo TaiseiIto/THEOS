8ba9c71eda88a53e06f72e26ec6d50cb2d816bc3
TaiseiIto <taisei1212@outlook.jp>
2023-03-04 22:18:08 +0900

    Print image handle and system table #8

--- src/bootx64/src/main.rs
+++ src/bootx64/src/main.rs
@@ -12,15 +12,18 @@ use {
 };
 
 #[no_mangle]
-fn efi_main(_image_handle: u64, _system_table: u64) -> u64 {
+fn efi_main(image_handle: u64, system_table: u64) -> u64 {
     println!("Hello, World!");
+    println!("image_handle = {:#018x}", image_handle);
+    println!("system_table = {:#018x}", system_table);
     loop {
         asm::hlt();
     }
 }
 
 #[panic_handler]
-fn panic(_panic: &PanicInfo) -> ! {
+fn panic(panic: &PanicInfo) -> ! {
+    println!("{}", panic);
     loop {
         asm::hlt();
     }
