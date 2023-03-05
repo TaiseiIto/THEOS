082a875911cafb0b4f16dd13401220c5ae90da79
TaiseiIto <taisei1212@outlook.jp>
2023-03-05 19:30:43 +0900

    Define a simple text output protocol #8

--- src/bootx64/src/uefi/protocols/simple_text_output.rs
+++ src/bootx64/src/uefi/protocols/simple_text_output.rs
@@ -1,6 +1,9 @@
 use {
     core::fmt,
-    super::super::types::status,
+    super::super::types::{
+        char16,
+        status,
+    },
 };
 
 // References
@@ -8,16 +11,20 @@ use {
 // 12.4 Simple Text Output Protocol
 pub struct SimpleTextOutput {
     reset: TextReset,
+    output_string: TextString,
 }
 
 impl fmt::Debug for SimpleTextOutput {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let reset: usize = self.reset as usize;
+        let output_string: usize = self.output_string as usize;
         write!(f, "SimpleTextOutput {{\n").expect("Can't print a simple text output protocol!");
         write!(f, "    reset: {:#x},\n", reset).expect("Can't print a simple text output protocol!");
+        write!(f, "    output_string: {:#x},\n", output_string).expect("Can't print a simple text output protocol!");
         write!(f, "}}")
     }
 }
 
 type TextReset = fn(&SimpleTextOutput, bool) -> status::Status;
+type TextString = fn(&SimpleTextOutput, char16::String) -> status::Status;
 
