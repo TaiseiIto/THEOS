81c0692391210382b9606d54443a84fc3fd11f42
TaiseiIto <taisei1212@outlook.jp>
2023-03-05 17:04:52 +0900

    Define simple text input protocol #8

--- src/bootx64/src/uefi/protocols/simple_text_input.rs
+++ src/bootx64/src/uefi/protocols/simple_text_input.rs
@@ -1,6 +1,11 @@
+use super::super::types::status;
+
 // References
 // https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf
 // 12.3 Simple Text Input Protocol
 pub struct SimpleTextInput {
+    reset: InputReset,
 }
+
+type InputReset = fn(*const SimpleTextInput, bool) -> status::Status;
 
