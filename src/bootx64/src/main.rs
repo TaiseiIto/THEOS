cca63eb87391159996ac79cba980eb500cf627d8
TaiseiIto <taisei1212@outlook.jp>
2023-03-06 13:28:05 +0900

    Define print macro #8

--- src/bootx64/src/main.rs
+++ src/bootx64/src/main.rs
@@ -19,15 +19,14 @@ use {
 };
 
 #[no_mangle]
-fn efi_main(image_handle: handle::Handle, system_table: &system::System) -> status::Status {
+fn efi_main(image_handle: handle::Handle, system_table: &mut system::System) -> status::Status {
     let mut com1 = serial::Serial::new(serial::COM1PORT, serial::BAUD);
     serial_println!(&mut com1, "Hello, World!");
     serial_println!(&mut com1, "image_handle = {:#x?}", image_handle);
     serial_println!(&mut com1, "system_table = {:#x?}", system_table);
     let status: status::Status = system_table.con_out.reset(false);
     serial_println!(&mut com1, "status = {}", status);
-    let status: status::Status = system_table.con_out.output_string("Hello, World!\n");
-    serial_println!(&mut com1, "status = {}", status);
+    uefi_println!(system_table, "Hello, World!");
     loop {
         asm::hlt();
     }
--- src/bootx64/src/uefi/tables/system.rs
+++ src/bootx64/src/uefi/tables/system.rs
@@ -1,16 +1,37 @@
-use super::{
-    header,
+use {
+    core::fmt::{
+        self,
+        Write,
+    },
     super::{
-        protocols::{
-            simple_text_input,
-            simple_text_output,
-        },
-        types::{
-            char16,
-            handle,
+        header,
+        super::{
+            protocols::{
+                simple_text_input,
+                simple_text_output,
+            },
+            types::{
+                char16,
+                handle,
+            },
         },
     },
 };
+
+#[macro_export]
+macro_rules! uefi_print {
+    ($system:expr, $($arg:tt)*) => ($crate::uefi::tables::system::print($system, format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! uefi_println {
+    ($system:expr, $fmt:expr) => (uefi_print!($system, concat!($fmt, "\n")));
+    ($system:expr, $fmt:expr, $($arg:tt)*) => (uefi_print!($system, concat!($fmt, "\n"), $($arg)*));
+}
+
+pub fn print(system: &mut System<'_>, args: fmt::Arguments) {
+    system.write_fmt(args).expect("Can't output to the screen!");
+}
 
 // References
 // https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf
@@ -25,5 +46,12 @@ pub struct System<'a> {
     con_in: &'a simple_text_input::SimpleTextInput<'a>,
     console_out_handle: handle::Handle<'a>,
     pub con_out: &'a simple_text_output::SimpleTextOutput<'a>,
+}
+
+impl Write for System<'_> {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.con_out.output_string(s);
+        Ok(())
+    }
 }
 
