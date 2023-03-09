0ee31581d307e6774544a992039b84b68ae67a53
TaiseiIto <taisei1212@outlook.jp>
2023-03-09 14:13:18 +0900

    Get memory map #13

--- src/bootx64/src/memory/map.rs
+++ src/bootx64/src/memory/map.rs
@@ -5,7 +5,7 @@ use super::super::uefi::{
 
 #[derive(Debug)]
 pub struct Buffer<'a> {
-    buffer: &'a u8,
+    buffer: &'a [u8],
     buffer_size: usize,
     key: usize,
     descriptor_size: usize,
@@ -13,11 +13,13 @@ pub struct Buffer<'a> {
 }
 
 impl<'a> Buffer<'a> {
-    pub fn new(buffer: &'a mut u8, mut buffer_size: usize, system: &system::System<'_>) -> Self {
+    pub fn new(buffer: &'a mut [u8], system: &system::System<'_>) -> Self {
+        let mut buffer_size: usize = buffer.len();
+        let buffer_address: &mut u8 = &mut buffer[0];
         let mut key: usize = 0;
         let mut descriptor_size: usize = 0;
         let mut descriptor_version: u32 = 0;
-        system.boot_services.get_memory_map(&mut buffer_size, buffer, &mut key, &mut descriptor_size, &mut descriptor_version);
+        system.boot_services.get_memory_map(&mut buffer_size, buffer_address, &mut key, &mut descriptor_size, &mut descriptor_version);
         Self {
             buffer,
             buffer_size,
