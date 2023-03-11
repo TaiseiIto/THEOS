2af80a2135c408132da2f55fb01487abf6a4013a
TaiseiIto <taisei1212@outlook.jp>
2023-03-11 21:18:05 +0900

    Revert "Free memory #15"
    This reverts commit f35e7aac62e933a5673f8afe0ad17871dbf5c716.

--- src/bootx64/src/allocator.rs
+++ src/bootx64/src/allocator.rs
@@ -1,14 +1,8 @@
 extern crate alloc;
 
 use {
-    alloc::{
-        alloc::Layout,
-        collections::btree_map,
-    },
-    core::{
-        alloc::GlobalAlloc,
-        cell,
-    },
+    alloc::alloc::Layout,
+    core::alloc::GlobalAlloc,
     super::uefi::{
         services::boot::memory_allocation,
         tables::system,
@@ -22,12 +16,10 @@ use {
 #[global_allocator]
 static mut ALLOCATOR: Allocator = Allocator {
     system: None,
-    provided_to_allocated: cell::UnsafeCell::new(btree_map::BTreeMap::new()),
 };
 
 pub struct Allocator {
-    system: Option<system::System<'static>>,
-    provided_to_allocated: cell::UnsafeCell<btree_map::BTreeMap<usize, usize>>,
+    system: Option<system::System<'static>>
 }
 
 impl Allocator {
@@ -59,33 +51,12 @@ unsafe impl GlobalAlloc for Allocator {
         }.expect("Can't allocate memory!");
         let allocated = allocated as *const void::Void;
         let allocated = allocated as usize;
-        let provided = ((allocated + align - 1) / align) * align;
-        self.provided_to_allocated
-            .get()
-            .as_mut()
-            .expect("Can't allocate memory!")
-            .insert(provided, allocated);
-        let provided = provided as *mut u8;
-        provided
+        let allocated = ((allocated + align - 1) / align) * align;
+        let allocated = allocated as *mut u8;
+        allocated
     }
 
-    unsafe fn dealloc(&self, pointer: *mut u8, _: Layout) {
-        let provided = pointer as usize;
-        let allocated: usize = *self.provided_to_allocated
-            .get()
-            .as_ref()
-            .expect("Can't free memory!")
-            .get(&provided)
-            .expect("Can't free memory!");
-        let allocated = allocated as *const void::Void;
-        let allocated = &*allocated;
-        match &self.system {
-            Some(system) => match system.boot_services.free_pool(allocated) {
-                status::SUCCESS => Ok(()),
-                _ => Err(()),
-            },
-            None => Err(()),
-        }.expect("Can't free memory!");
+    unsafe fn dealloc(&self, _: *mut u8, _: Layout) {
     }
 }
 
