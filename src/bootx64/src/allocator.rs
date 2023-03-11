be8469d6a5bcff8646ca55dbc78ea89f24808273
TaiseiIto <taisei1212@outlook.jp>
2023-03-11 20:06:03 +0900

    Prepare an allocator #15

--- src/bootx64/src/allocator.rs
+++ src/bootx64/src/allocator.rs
@@ -1,15 +1,62 @@
-use super::uefi::tables::system;
+extern crate alloc;
+
+use {
+    alloc::alloc::Layout,
+    core::alloc::GlobalAlloc,
+    super::uefi::{
+        services::boot::memory_allocation,
+        tables::system,
+        types::{
+            status,
+            void,
+        },
+    },
+};
+
+#[global_allocator]
+static mut ALLOCATOR: Allocator = Allocator {
+    system: None,
+};
 
 pub struct Allocator {
+    system: Option<system::System<'static>>
 }
 
 impl Allocator {
     pub fn set_system(system: system::System<'static>) {
         unsafe {
-            SYSTEM = Some(system);
+            ALLOCATOR.system = Some(system);
         }
     }
 }
 
-static mut SYSTEM: Option<system::System> = None;
+unsafe impl GlobalAlloc for Allocator {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        let requested_size: usize = layout.size();
+        let align: usize = layout.align();
+        let memory_type = memory_allocation::MemoryType::LoaderData;
+        let allocated_size: usize = align + requested_size - 1;
+        let allocated = void::Void::new();
+        let mut allocated = &allocated;
+        match &self.system {
+            Some(system) => match system.boot_services.allocate_pool(
+                memory_type,
+                allocated_size,
+                &mut allocated,
+            ) {
+                status::SUCCESS => Ok(()),
+                _ => Err(()),
+            },
+            None => Err(()),
+        }.expect("Can't allocate memory!");
+        let allocated = allocated as *const void::Void;
+        let allocated = allocated as usize;
+        let allocated = ((allocated + align - 1) / align) * align;
+        let allocated = allocated as *mut u8;
+        allocated
+    }
+
+    unsafe fn dealloc(&self, _: *mut u8, _: Layout) {
+    }
+}
 
