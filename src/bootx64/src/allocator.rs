3bb4119d7d8f9f9f41873ebe2e079cdb254f543a
TaiseiIto <taisei1212@outlook.jp>
2023-03-11 21:50:45 +0900

    Prepare an address map #15

--- src/bootx64/src/allocator.rs
+++ src/bootx64/src/allocator.rs
@@ -60,3 +60,55 @@ unsafe impl GlobalAlloc for Allocator {
     }
 }
 
+#[derive(Clone, Copy)]
+struct AddressPair {
+    allocated: usize,
+    provided: usize,
+}
+
+impl AddressPair {
+    fn new(allocated: usize, provided: usize) -> Self {
+        Self {
+            allocated,
+            provided,
+        }
+    }
+}
+
+struct AddressMap {
+    pairs: [Option<AddressPair>; ADDRESS_PAIRS],
+}
+
+const ADDRESS_PAIRS: usize = 0x400;
+
+impl AddressMap {
+    fn new() -> Self {
+        let pairs: [Option<AddressPair>; ADDRESS_PAIRS] = [None; ADDRESS_PAIRS];
+        Self {
+            pairs,
+        }
+    }
+
+    fn insert(&mut self, allocated: usize, provided: usize) {
+        *self.pairs
+            .iter_mut()
+            .find(|pair| pair.is_none())
+            .expect("Can't insert an address pair to an address map!")
+            = Some(AddressPair::new(allocated, provided));
+    }
+
+    fn find(&self, provided: usize) -> Option<usize> {
+        self.pairs
+            .iter()
+            .filter_map(|pair| *pair)
+            .find_map(|pair| if let AddressPair {
+                allocated,
+                provided,
+            } = pair {
+                Some(allocated)
+            } else {
+                None
+            })
+    }
+}
+
