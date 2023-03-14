7d5c3b998b06606bcfb833fc3527ab67088d5de5
TaiseiIto <taisei1212@outlook.jp>
2023-03-14 09:24:31 +0900

    Get CPUID #15

--- src/bootx64/src/asm/cpuid/eax0x00000001.rs
+++ src/bootx64/src/asm/cpuid/eax0x00000001.rs
@@ -2,6 +2,26 @@ use super::CpuidOutRegisters;
 
 #[derive(Debug)]
 pub struct Eax0x00000001 {
+    eax: Eax,
+}
+
+impl Eax0x00000001 {
+    pub fn new() -> Self {
+        let CpuidOutRegisters {
+            eax,
+            ebx: _,
+            edx: _,
+            ecx: _,
+        } = CpuidOutRegisters::cpuid(1);
+        let eax: Eax = eax.into();
+        Self {
+            eax,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Eax {
     stepping_id: u8,
     model: u8,
     family_id: u8,
@@ -10,7 +30,7 @@ pub struct Eax0x00000001 {
     extended_family_id: u8,
 }
 
-impl Eax0x00000001 {
+impl Eax {
     const STEPPING_ID_SHIFT: usize = 0;
     const MODEL_SHIFT: usize = 4;
     const FAMILY_ID_SHIFT: usize = 8;
@@ -38,14 +58,10 @@ impl Eax0x00000001 {
     const PROCESSOR_TYPE_MASK: u32 = (((1 << Self::PROCESSOR_TYPE_LENGTH) - 1) << Self::PROCESSOR_TYPE_SHIFT) as u32;
     const EXTENDED_MODEL_ID_MASK: u32 = (((1 << Self::EXTENDED_MODEL_ID_LENGTH) - 1) << Self::EXTENDED_MODEL_ID_SHIFT) as u32;
     const EXTENDED_FAMILY_ID_MASK: u32 = (((1 << Self::EXTENDED_FAMILY_ID_LENGTH) - 1) << Self::EXTENDED_FAMILY_ID_SHIFT) as u32;
+}
     
-    pub fn new() -> Self {
-        let CpuidOutRegisters {
-            eax,
-            ebx: _,
-            edx: _,
-            ecx: _,
-        } = CpuidOutRegisters::cpuid(1);
+impl From<u32> for Eax {
+    fn from(eax: u32) -> Self {
         let stepping_id = ((eax & Self::STEPPING_ID_MASK) >> Self::STEPPING_ID_SHIFT) as u8;
         let model = ((eax & Self::MODEL_MASK) >> Self::MODEL_SHIFT) as u8;
         let family_id = ((eax & Self::FAMILY_ID_MASK) >> Self::FAMILY_ID_SHIFT) as u8;
