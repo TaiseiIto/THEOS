0cb1c1f19162aaf8f74e18864ba392966cb9b733
TaiseiIto <taisei1212@outlook.jp>
2023-03-15 21:32:47 +0900

    Get CPUID #15

--- src/bootx64/src/asm/cpuid/eax0x00000005.rs
+++ src/bootx64/src/asm/cpuid/eax0x00000005.rs
@@ -8,6 +8,7 @@ use super::{
 pub struct Eax0x00000005 {
     eax: Eax,
     ebx: Ebx,
+    edx: Edx,
 }
 
 impl Eax0x00000005 {
@@ -16,14 +17,16 @@ impl Eax0x00000005 {
             let CpuidOutRegisters {
                 eax,
                 ebx,
-                edx: _,
+                edx,
                 ecx: _,
             } = CpuidOutRegisters::cpuid(5);
             let eax: Eax = eax.into();
             let ebx: Ebx = ebx.into();
+            let edx: Edx = edx.into();
             Some(Self {
                 eax,
                 ebx,
+                edx,
             })
         } else {
             None
@@ -61,3 +64,20 @@ impl From<u32> for Ebx {
     }
 }
 
+#[allow(dead_code)]
+#[derive(Debug)]
+pub struct Edx {
+    number_of_sub_c_states: [u8; 8],
+}
+
+impl From<u32> for Edx {
+    fn from(edx: u32) -> Self {
+        let mut number_of_sub_c_states: [u8; 8] = [0; 8];
+        for i in 0..8 {
+            number_of_sub_c_states[i] = ((edx >> (4 * i)) & 0x0f) as u8;
+        }
+        Self {
+            number_of_sub_c_states,
+        }
+    }
+}
