8afc326f82addcfe6824e82fa8940e544361396a
TaiseiIto <taisei1212@outlook.jp>
2023-03-15 21:47:46 +0900

    Get CPUID #15

--- src/bootx64/src/asm/cpuid/eax0x00000005.rs
+++ src/bootx64/src/asm/cpuid/eax0x00000005.rs
@@ -9,6 +9,7 @@ pub struct Eax0x00000005 {
     eax: Eax,
     ebx: Ebx,
     edx: Edx,
+    ecx: Ecx,
 }
 
 impl Eax0x00000005 {
@@ -18,15 +19,17 @@ impl Eax0x00000005 {
                 eax,
                 ebx,
                 edx,
-                ecx: _,
+                ecx,
             } = CpuidOutRegisters::cpuid(5);
             let eax: Eax = eax.into();
             let ebx: Ebx = ebx.into();
             let edx: Edx = edx.into();
+            let ecx: Ecx = ecx.into();
             Some(Self {
                 eax,
                 ebx,
                 edx,
+                ecx,
             })
         } else {
             None
@@ -81,3 +84,30 @@ impl From<u32> for Edx {
         }
     }
 }
+
+#[allow(dead_code)]
+#[derive(Debug)]
+pub struct Ecx {
+    enumeration_of_monitor_mwait_extensions_suppored: bool,
+    supports_treating_interrupts_as_break_event_for_mwait: bool,
+}
+
+impl Ecx {
+    const ENUMERATION_OF_MONITOR_MWAIT_EXTENSIONS_SUPPORED_SHIFT: usize = 0;
+    const SUPPORTS_TREATING_INTERRUPTS_AS_BREAK_EVENT_FOR_MWAIT_SHIFT: usize = 1;
+
+    const ENUMERATION_OF_MONITOR_MWAIT_EXTENSIONS_SUPPORED_MASK: u32 = 1 << Self::ENUMERATION_OF_MONITOR_MWAIT_EXTENSIONS_SUPPORED_SHIFT;
+    const SUPPORTS_TREATING_INTERRUPTS_AS_BREAK_EVENT_FOR_MWAIT_MASK: u32 = 1 << Self::SUPPORTS_TREATING_INTERRUPTS_AS_BREAK_EVENT_FOR_MWAIT_SHIFT;
+}
+
+impl From<u32> for Ecx {
+    fn from(ecx: u32) -> Self {
+        let enumeration_of_monitor_mwait_extensions_suppored = ecx & Self::ENUMERATION_OF_MONITOR_MWAIT_EXTENSIONS_SUPPORED_MASK != 0;
+        let supports_treating_interrupts_as_break_event_for_mwait = ecx & Self::SUPPORTS_TREATING_INTERRUPTS_AS_BREAK_EVENT_FOR_MWAIT_MASK != 0;
+        Self {
+            enumeration_of_monitor_mwait_extensions_suppored,
+            supports_treating_interrupts_as_break_event_for_mwait,
+        }
+    }
+}
+
