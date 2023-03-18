c6154ec6dff2bb3126936889e94e482686ea261b
TaiseiIto <taisei1212@outlook.jp>
2023-03-18 16:27:08 +0900

    Build kernel #15

--- src/kernel/src/main.rs
+++ src/kernel/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {
+    }
 }
+
+#[panic_handler]
+fn panic(_panic: &PanicInfo) -> ! {
+    loop {
+    }
+}
+
