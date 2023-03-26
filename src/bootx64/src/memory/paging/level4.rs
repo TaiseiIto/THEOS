2aacaf71aef1038762cdb197d0187d7de47e34da
TaiseiIto <taisei1212@outlook.jp>
2023-03-26 21:00:37 +0900

    Paging entry has own pointer #15

--- src/bootx64/src/memory/paging/level4.rs
+++ src/bootx64/src/memory/paging/level4.rs
@@ -8,7 +8,6 @@ use alloc::vec::Vec;
 pub struct Cr3<'a> {
     pwt: bool,
     pcd: bool,
-    page_map_level_4_table: &'a [u64; ENTRIES],
     page_map_level_4_entries: Vec<PageMapLevel4Entry<'a>>,
 }
 
@@ -28,18 +27,17 @@ impl From<u64> for Cr3<'_> {
         let pwt: bool = cr3 & Self::PWT_MASK != 0;
         let pcd: bool = cr3 & Self::PCD_MASK != 0;
         let page_map_level_4_table: u64 = cr3 & Self::PAGE_DIRECTORY_BASE_MASK;
-        let page_map_level_4_table: *const [u64; ENTRIES] = page_map_level_4_table as *const [u64; ENTRIES];
-        let page_map_level_4_table: &[u64; ENTRIES] = unsafe {
-            &*page_map_level_4_table
+        let page_map_level_4_table: *mut [u64; ENTRIES] = page_map_level_4_table as *mut [u64; ENTRIES];
+        let page_map_level_4_table: &mut [u64; ENTRIES] = unsafe {
+            &mut *page_map_level_4_table
         };
         let page_map_level_4_entries: Vec<PageMapLevel4Entry> = page_map_level_4_table
-            .iter()
-            .filter_map(|page_map_level_4_entry| PageMapLevel4Entry::read(*page_map_level_4_entry))
+            .iter_mut()
+            .filter_map(|page_map_level_4_entry| PageMapLevel4Entry::read(page_map_level_4_entry))
             .collect();
         Self {
             pwt,
             pcd,
-            page_map_level_4_table,
             page_map_level_4_entries,
         }
 	}
@@ -48,6 +46,7 @@ impl From<u64> for Cr3<'_> {
 #[allow(dead_code)]
 #[derive(Debug)]
 pub struct PageMapLevel4Entry<'a> {
+    page_map_level_4_entry: &'a mut u64,
     writable: bool,
     user_mode_access: bool,
     page_write_through: bool,
@@ -59,7 +58,7 @@ pub struct PageMapLevel4Entry<'a> {
     execute_disable: bool,
 }
 
-impl PageMapLevel4Entry<'_> {
+impl<'a> PageMapLevel4Entry<'a> {
     const PRESENT_SHIFT: usize = 0;
     const WRITABLE_SHIFT: usize = 1;
     const USER_MODE_ACCESS_SHIFT: usize = 2;
@@ -81,15 +80,15 @@ impl PageMapLevel4Entry<'_> {
     const PAGE_DIRECTORY_POINTER_TABLE_MASK: u64 = (1 << Self::PAGE_DIRECTORY_POINTER_TABLE_SHIFT_END) - (1 << Self::PAGE_DIRECTORY_POINTER_TABLE_SHIFT_BEGIN);
     const EXECUTE_DISABLE_MASK: u64 = 1 << Self::EXECUTE_DISABLE_SHIFT;
 
-    fn read(page_map_level_4_entry: u64) -> Option<Self> {
-        if page_map_level_4_entry & Self::PRESENT_MASK != 0 {
-            let writable: bool = page_map_level_4_entry & Self::WRITABLE_MASK != 0;
-            let user_mode_access: bool = page_map_level_4_entry & Self::USER_MODE_ACCESS_MASK != 0;
-            let page_write_through: bool = page_map_level_4_entry & Self::PAGE_WRITE_THROUGH_MASK != 0;
-            let page_cache_disable: bool = page_map_level_4_entry & Self::PAGE_CACHE_DISABLE_MASK != 0;
-            let accessed: bool = page_map_level_4_entry & Self::ACCESSED_MASK != 0;
-            let restart: bool = page_map_level_4_entry & Self::RESTART_MASK != 0;
-            let page_directory_pointer_table: u64 = page_map_level_4_entry & Self::PAGE_DIRECTORY_POINTER_TABLE_MASK;
+    fn read(page_map_level_4_entry: &'a mut u64) -> Option<Self> {
+        if *page_map_level_4_entry & Self::PRESENT_MASK != 0 {
+            let writable: bool = *page_map_level_4_entry & Self::WRITABLE_MASK != 0;
+            let user_mode_access: bool = *page_map_level_4_entry & Self::USER_MODE_ACCESS_MASK != 0;
+            let page_write_through: bool = *page_map_level_4_entry & Self::PAGE_WRITE_THROUGH_MASK != 0;
+            let page_cache_disable: bool = *page_map_level_4_entry & Self::PAGE_CACHE_DISABLE_MASK != 0;
+            let accessed: bool = *page_map_level_4_entry & Self::ACCESSED_MASK != 0;
+            let restart: bool = *page_map_level_4_entry & Self::RESTART_MASK != 0;
+            let page_directory_pointer_table: u64 = *page_map_level_4_entry & Self::PAGE_DIRECTORY_POINTER_TABLE_MASK;
             let page_directory_pointer_table: *const [u64; ENTRIES] = page_directory_pointer_table as *const [u64; ENTRIES];
             let page_directory_pointer_table: &[u64; ENTRIES] = unsafe {
                 &*page_directory_pointer_table
@@ -98,8 +97,9 @@ impl PageMapLevel4Entry<'_> {
                 .iter()
                 .filter_map(|page_directory_pointer_entry| PageDirectoryPointerEntry::read(*page_directory_pointer_entry))
                 .collect();
-            let execute_disable: bool = page_map_level_4_entry & Self::EXECUTE_DISABLE_MASK != 0;
+            let execute_disable: bool = *page_map_level_4_entry & Self::EXECUTE_DISABLE_MASK != 0;
             Some(Self {
+                page_map_level_4_entry,
                 writable,
                 user_mode_access,
                 page_write_through,
