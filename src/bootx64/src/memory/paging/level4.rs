9462468b21864dcceaf0c50f8554eee5b08a6bcb
TaiseiIto <taisei1212@outlook.jp>
2023-03-27 10:16:32 +0900

    Set physical address #15

--- src/bootx64/src/memory/paging/level4.rs
+++ src/bootx64/src/memory/paging/level4.rs
@@ -392,6 +392,85 @@ impl<'a> PageEntry<'a> {
     const PROTECTION_KEY_MASK: u64 = (1 << Self::PROTECTION_KEY_SHIFT_END) - (1 << Self::PROTECTION_KEY_SHIFT_BEGIN);
     const EXECUTE_DISABLE_MASK: u64 = 1 << Self::EXECUTE_DISABLE_SHIFT;
 
+    fn new(
+        page_entry: &'a mut u64,
+        writable: bool,
+        user_mode_access: bool,
+        page_write_through: bool,
+        page_cache_disable: bool,
+        accessed: bool,
+        dirty: bool,
+        page_attribute_table: bool,
+        restart: bool,
+        physical_address: u64,
+        protection_key: u8,
+        execute_disable: bool,
+    ) -> Self {
+        let new_page_entry = Self {
+            page_entry,
+            writable,
+            user_mode_access,
+            page_write_through,
+            page_cache_disable,
+            accessed,
+            dirty,
+            page_attribute_table,
+            restart,
+            physical_address,
+            protection_key,
+            execute_disable,
+        };
+        let writable: u64 = if writable {
+            1 << Self::WRITABLE_SHIFT
+        } else {
+            0
+        };
+        let user_mode_access: u64 = if user_mode_access {
+            1 << Self::USER_MODE_ACCESS_SHIFT
+        } else {
+            0
+        };
+        let page_write_through: u64 = if page_write_through {
+            1 << Self::PAGE_WRITE_THROUGH_SHIFT
+        } else {
+            0
+        };
+        let page_cache_disable: u64 = if page_cache_disable {
+            1 << Self::PAGE_CACHE_DISABLE_SHIFT
+        } else {
+            0
+        };
+        let accessed: u64 = if accessed {
+            1 << Self::ACCESSED_SHIFT
+        } else {
+            0
+        };
+        let dirty: u64 = if dirty {
+            1 << Self::DIRTY_SHIFT
+        } else {
+            0
+        };
+        let page_attribute_table: u64 = if page_attribute_table {
+            1 << Self::PAGE_ATTRIBUTE_TABLE_SHIFT
+        } else {
+            0
+        };
+        let restart: u64 = if restart {
+            1 << Self::RESTART_SHIFT
+        } else {
+            0
+        };
+        let physical_address: u64 = physical_address & Self::PHYSICAL_ADDRESS_MASK;
+        let protection_key: u64 = (protection_key as u64) << Self::PROTECTION_KEY_SHIFT_BEGIN;
+        let execute_disable: u64 = if execute_disable {
+            1 << Self::EXECUTE_DISABLE_SHIFT
+        } else {
+            0
+        };
+        *new_page_entry.page_entry = writable | user_mode_access | page_write_through | page_cache_disable | accessed | dirty | page_attribute_table | restart | physical_address | protection_key | execute_disable;
+        new_page_entry
+    }
+
     fn read(page_entry: &'a mut u64) -> Option<Self> {
         if *page_entry & Self::PRESENT_MASK != 0 {
             let writable: bool = *page_entry & Self::WRITABLE_MASK != 0;
