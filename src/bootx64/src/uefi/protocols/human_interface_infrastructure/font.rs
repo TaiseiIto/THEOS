1f7b5ed4d718b2641a4e898afa0bbff222cd19c4
TaiseiIto <taisei1212@outlook.jp>
2023-11-06 21:59:04 +0900

    Get font from UEFI #25

--- src/bootx64/src/uefi/protocols/human_interface_infrastructure/font.rs
+++ src/bootx64/src/uefi/protocols/human_interface_infrastructure/font.rs
@@ -30,6 +30,7 @@ pub struct Font {
     string_to_image: StringToImage,
     string_id_to_image: StringIdToImage,
     get_glyph: GetGlyph,
+    get_font_info: GetFontInfo,
 }
 
 impl Font {
@@ -68,17 +69,28 @@ impl Font {
     }
 }
 
+// EFI_HII_STRING_TO_IMAGE
 #[derive(WrappedFunction)]
 #[repr(C)]
 struct StringToImage(pub extern "efiapi" fn(&Font, OutFlags, char16::String, &font_ex::FontDisplayInfo, &mut &font_ex::ImageOutput, usize, usize, &mut &RowInfo, &mut usize, &mut usize) -> status::Status);
 
+// EFI_HII_STRING_ID_TO_IMAGE
 #[derive(WrappedFunction)]
 #[repr(C)]
 struct StringIdToImage(pub extern "efiapi" fn(&Font, OutFlags, database::Handle, StringId, &char8::Char8, &font_ex::FontDisplayInfo, &mut &font_ex::ImageOutput, usize, usize, &mut &RowInfo, &mut usize, &mut usize) -> status::Status);
 
+// EFI_HII_GET_GLYPH
 #[derive(WrappedFunction)]
 #[repr(C)]
 struct GetGlyph(pub extern "efiapi" fn(&Font, char16::Char16, &font_ex::FontDisplayInfo, &mut &font_ex::ImageOutput, &mut usize) -> status::Status);
+
+// EFI_HII_GET_FONT_INFO
+#[derive(WrappedFunction)]
+#[repr(C)]
+struct GetFontInfo(pub extern "efiapi" fn(&Font, &mut FontHandle<'_>, &font_ex::FontDisplayInfo, &mut &font_ex::FontDisplayInfo, char16::String) -> status::Status);
+
+// EFI_FONT_HANDLE
+pub type FontHandle<'a> = &'a void::Void;
 
 // EFI_HII_OUT_FLAGS
 pub type OutFlags = u32;
