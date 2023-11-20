36069d6a4390f01729c5c58613068294326d41bb
TaiseiIto <taisei1212@outlook.jp>
2023-11-20 20:19:06 +0900

    Implement the global allocator #27

--- src/kernel/src/allocator.rs
+++ src/kernel/src/allocator.rs
@@ -224,19 +224,16 @@ impl<'a> ChunkList<'a> {
     }
 
     fn dealloc(&'a mut self, address: usize) {
-        let (deallocated_chunk, previous_chunk, next_chunk): (&mut Chunk, Option<&mut Option<Chunk>>, Option<&mut Option<Chunk>>) = self.get_deallocated_chunk(address);
+        let (deallocated_chunk, previous_chunk, next_chunk): (&mut Option<Chunk>, Option<&mut Option<Chunk>>, Option<&mut Option<Chunk>>) = self.get_deallocated_chunk(address);
         serial_println!("deallocated_chunk = {:#x?}", deallocated_chunk);
         serial_println!("previous_chunk = {:#x?}", previous_chunk);
         serial_println!("next_chunk = {:#x?}", next_chunk);
         panic!("ChunkList.dealloc is not implemented!");
     }
 
-    fn get_deallocated_chunk(&'a mut self, address: usize) -> (&mut Chunk, Option<&mut Option<Chunk>>, Option<&mut Option<Chunk>>) {
+    fn get_deallocated_chunk(&'a mut self, address: usize) -> (&mut Option<Chunk>, Option<&mut Option<Chunk>>, Option<&mut Option<Chunk>>) {
         let (deallocated_chunk, previous_chunk, next_chunk): (Option<&mut Option<Chunk>>, Option<&mut Option<Chunk>>, Option<&mut Option<Chunk>>) = self.scan_deallocated_chunk(address, None, None, None);
-        let deallocated_chunk: &mut Chunk = deallocated_chunk
-            .expect("Can't find the deallocated chunk!")
-            .as_mut()
-            .expect("Can't find the deallocated chunk!");
+        let deallocated_chunk: &mut Option<Chunk> = deallocated_chunk.expect("Can't find the deallocated chunk!");
         (deallocated_chunk, previous_chunk, next_chunk)
     }
 
