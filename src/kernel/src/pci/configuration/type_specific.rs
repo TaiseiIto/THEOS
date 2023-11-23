2b8ed5d9067d0aa4b6a328924d1df2b7966371ae
TaiseiIto <taisei1212@outlook.jp>
2023-11-23 22:18:24 +0900

    Get a host bridge configuration #29

--- src/kernel/src/pci/configuration/type_specific.rs
+++ src/kernel/src/pci/configuration/type_specific.rs
@@ -1,6 +1,7 @@
 extern crate alloc;
 
 mod base_address;
+mod expansion_rom_base_address;
 
 use {
     alloc::vec::Vec,
@@ -22,6 +23,7 @@ pub enum Registers {
         cardbus_cis_pointer: u32,
         subsystem_vendor_id: u16,
         subsystem_id: u16,
+        expansion_rom_base_address: expansion_rom_base_address::Register,
     },
     Type1,
     Reserved,
@@ -39,6 +41,9 @@ impl Registers {
     const SUBSYSTEM_ID_BEGIN: usize = Self::SUBSYSTEM_VENDOR_ID_END;
     const SUBSYSTEM_ID_LENGTH: usize = mem::size_of::<u16>();
     const SUBSYSTEM_ID_END: usize = Self::SUBSYSTEM_ID_BEGIN + Self::SUBSYSTEM_ID_LENGTH;
+    const EXPANSION_ROM_BASE_ADDRESS_BEGIN: usize = Self::SUBSYSTEM_ID_END;
+    const EXPANSION_ROM_BASE_ADDRESS_LENGTH: usize = mem::size_of::<u32>();
+    const EXPANSION_ROM_BASE_ADDRESS_END: usize = Self::EXPANSION_ROM_BASE_ADDRESS_BEGIN + Self::EXPANSION_ROM_BASE_ADDRESS_LENGTH;
 
     pub fn new(header_layout: &header_type::HeaderLayout, configuration: &[u8; CONFIGURATION_SIZE]) -> Self {
         match header_layout {
@@ -66,11 +71,16 @@ impl Registers {
                     .try_into()
                     .expect("Can't get a PCI device subsystem ID!");
                 let subsystem_id: u16 = u16::from_le_bytes(subsystem_id);
+                let expansion_rom_base_address: [u8; Self::EXPANSION_ROM_BASE_ADDRESS_LENGTH] = configuration[Self::EXPANSION_ROM_BASE_ADDRESS_BEGIN..Self::EXPANSION_ROM_BASE_ADDRESS_END]
+                    .try_into()
+                    .expect("Can't get a expansion ROM base address!");
+                let expansion_rom_base_address: expansion_rom_base_address::Register = u32::from_le_bytes(expansion_rom_base_address).into();
                 Self::Type0 {
                     base_address_registers,
                     cardbus_cis_pointer,
                     subsystem_vendor_id,
                     subsystem_id,
+                    expansion_rom_base_address,
                 }
             },
             header_type::HeaderLayout::Type1 => Self::Type1,
