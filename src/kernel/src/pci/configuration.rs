7d9a0ed689b3aef8cfba55f5f211b1099ce80a6e
TaiseiIto <taisei1212@outlook.jp>
2023-11-28 21:05:11 +0900

    Define all PCI devices #29

--- src/kernel/src/pci/configuration.rs
+++ src/kernel/src/pci/configuration.rs
@@ -227,10 +227,165 @@ impl From<[u8; CONFIGURATION_SIZE]> for Device {
 }
 
 // https://pcisig.com/sites/default/files/files/PCI_Code-ID_r_1_11__v24_Jan_2019.pdf
+#[allow(dead_code)]
 #[derive(Debug)]
 enum ClassCode {
-    AllCurrentlyImplementedDevice,
-    VGACompatibleDevice,
+    AllCurrentlyImplemented,                    // 00 00 00
+    VGACompatibleDevice,                        // 00 01 00
+    SCSI,                                       // 01 00 xx
+    IDE,                                        // 01 01 xx
+    FloppyDisk,                                 // 01 02 00
+    IPIBus,                                     // 01 03 00
+    RAID,                                       // 01 04 00
+    ATASingleStepping,                          // 01 05 20
+    ATAContinuousOperation,                     // 01 05 30
+    SerialATAVendorSpecific,                    // 01 06 00
+    SerialATAAHCI,                              // 01 06 01
+    SerialStorage,                              // 01 06 02
+    SerialAttachedSCSI,                         // 01 07 00
+    Obsolete,                                   // 01 07 01
+    NoneVolatileMemorySubsystemVendorSpecific,  // 01 08 00
+    NoneVolatileMemorySubsystemNVMHCI,          // 01 08 01
+    NVMExpressIO,                               // 01 08 02
+    NVMExpressAdministrative,                   // 01 08 03
+    UniversalFlashStorageVendorSpecific,        // 01 09 00
+    UniversalFlashStorageHostController,        // 01 09 01
+    OtherMassStorage,                           // 01 80 00
+    Ethernet,                                   // 02 00 00
+    TokenRing,                                  // 02 01 00
+    FDDI,                                       // 02 02 00
+    ATM,                                        // 02 03 00
+    ISDN,                                       // 02 04 00
+    WorldFip,                                   // 02 05 00
+    PICMG,                                      // 02 06 xx
+    InfiniBand,                                 // 02 07 00
+    HostFabric,                                 // 02 08 00
+    OtherNetwork,                               // 02 80 00
+    VGACompatibleController,                    // 03 00 00
+    Display8514Compatible,                      // 03 00 01
+    XGA,                                        // 03 01 00
+    Display3D,                                  // 03 02 00
+    OtherDisplay,                               // 03 80 00
+    Video,                                      // 04 00 00
+    AudioDevice,                                // 04 01 00
+    ComputerTelephony,                          // 05 02 00
+    HighDefinitionAudio,                        // 04 03 00
+    HighDefinitionAudioVendorSpecific,          // 04 03 80
+    OtherMultimedia,                            // 04 80 00
+    RAM,                                        // 05 00 00
+    Flash,                                      // 05 01 00
+    OtherMemory,                                // 05 80 00
+    HostBridge,                                 // 06 00 00
+    ISABridge,                                  // 06 01 00
+    EISABridge,                                 // 06 02 00
+    MCABridge,                                  // 06 03 00
+    PCI2PCIBridge,                              // 06 04 00
+    SubtractiveDecodePCI2PCIBridge,             // 06 04 01
+    PCMCIABridge,                               // 06 05 00
+    NuBusBridge,                                // 06 06 00
+    CardBusBridge,                              // 06 07 00
+    RACEwayBridge,                              // 06 08 xx
+    SemiTransparentPCI2PCIBridgePrimary,        // 06 09 40
+    SemiTransparentPCI2PCIBridgeSecondary,      // 06 09 80
+    InfiniBand2PCIHostBridge,                   // 06 0a 00
+    AdvancedSwitching2PCIHostBridgeCustom,      // 06 0b 00
+    AdvancedSwitching2PCIHostBridgeASISIG,      // 06 0b 01
+    OtherBridge,                                // 06 80 00
+    SerialGenericXTCompatible,                  // 07 00 00
+    Serial16450Compatible,                      // 07 00 01
+    Serial16550Compatible,                      // 07 00 02
+    Serial16650Compatible,                      // 07 00 03
+    Serial16750Compatible,                      // 07 00 04
+    Serial16850Compatible,                      // 07 00 05
+    Serial16950Compatible,                      // 07 00 06
+    ParallelPort,                               // 07 01 00
+    BidirectionalParallelPort,                  // 07 01 01
+    ECP1XCompliantParallelPort,                 // 07 01 02
+    IEEE1284Controller,                         // 07 01 03
+    IEEE1284TargetDevice,                       // 07 01 fe
+    MultiportSerial,                            // 07 02 00
+    GenericModem,                               // 07 03 00
+    HayesCompatibleModem16450Compatible,        // 07 03 01
+    HayesCompatibleModem16550Compatible,        // 07 03 01
+    HayesCompatibleModem16650Compatible,        // 07 03 01
+    HayesCompatibleModem16750Compatible,        // 07 03 01
+    GPIB,                                       // 07 04 00
+    SmartCard,                                  // 07 05 00
+    OtherCommunications,                        // 07 80 00
+    Generic8259PIC,                             // 08 00 00
+    ISAPIC,                                     // 08 00 01
+    EISAPIC,                                    // 08 00 02
+    IOAPICInterrupt,                            // 08 00 10
+    IOxAPICInterrupt,                           // 08 00 20
+    Generic8237DMA,                             // 08 01 00
+    ISADMA,                                     // 08 01 01
+    EISADMA,                                    // 08 01 02
+    Generic8254SystemTimer,                     // 08 02 00
+    ISASystemTimer,                             // 08 02 01
+    EISASystemTimer,                            // 08 02 02
+    HighPerformanceEventTimer,                  // 08 02 03
+    GenericRTC,                                 // 08 03 00
+    ISARTC,                                     // 08 03 01
+    GenericPCIHotPlug,                          // 08 04 00
+    SDHost,                                     // 08 05 00
+    IOMMU,                                      // 08 06 00
+    RootComplexEvent,                           // 08 07 00
+    OtherSystemPeripheral,                      // 08 80 00
+    Kerboard,                                   // 09 00 00
+    Digitizer,                                  // 09 01 00
+    Mouse,                                      // 09 02 00
+    Scanner,                                    // 09 03 00
+    GenericGameport,                            // 09 04 00
+    Gameport,                                   // 09 04 10
+    OtherInput,                                 // 09 80 00
+    GenericDockingStation,                      // 0a 00 00
+    OtherDockingStation,                        // 0a 80 00
+    Processor386,                               // 0b 00 00
+    Processor486,                               // 0b 01 00
+    Pentium,                                    // 0b 02 00
+    Alpha,                                      // 0b 10 00
+    PowerPC,                                    // 0b 20 00
+    MIPS,                                       // 0b 30 00
+    Coprocessor,                                // 0b 40 00
+    OtherProcessor,                             // 0b 80 00
+    IEEE1394,                                   // 0c 00 00
+    IEEE1394OpenHCI,                            // 0c 00 10
+    ACCESSBus,                                  // 0c 01 00
+    SSA,                                        // 0c 02 00
+    USBUHCI,                                    // 0c 03 00
+    USBOHCI,                                    // 0c 03 10
+    USBEHCI,                                    // 0c 03 20
+    USBxHCI,                                    // 0c 03 30
+    USBNoSpecificProgrammingInterface,          // 0c 03 80
+    USBNoHostController,                        // 0c 03 fe
+    FibreChannel,                               // 0c 04 00
+    SMBus,                                      // 0c 05 00
+    InfiniBandDeprecated,                       // 0c 06 00
+    IPMISMIC,                                   // 0c 07 00
+    IPMIKeyboardControllerStyle,                // 0c 07 01
+    IPMIBlockTransfer,                          // 0c 07 02
+    SERCOS,                                     // 0c 08 00
+    CANbus,                                     // 0c 09 00
+    MIPII3C,                                    // 0c 0a 00
+    OtherSerialBus,                             // 0c 80 00
+    IRDA,                                       // 0d 00 00
+    ConsumerIR,                                 // 0d 01 00
+    UWBRadio,                                   // 0d 01 10
+    RF,                                         // 0d 10 00
+    Bluetooth,                                  // 0d 11 00
+    Broadband,                                  // 0d 12 00
+    Ethernet80211a,                             // 0d 20 00
+    Ethernet20811b,                             // 0d 21 00
+    Cellular,                                   // 0c 40 00
+    CellularPlusEthernet,                       // 0d 41 00
+    OtherWireless,                              // 0d 80 00
+    IntelligentIO,                              // 0e 00 xx
+    MessageFIFO,                                // 0e 00 00
+    TV,                                         // 0f 01 00
+    Audio,                                      // 0f 02 00
+    Voice,                                      // 0f 03 00
+    Data,                                       // 0f 04 00
+    OtherSatelliteCommunication,                // 0f 80 00
     Other {
         base_class: u8,
         sub_class: u8,
@@ -241,7 +396,7 @@ enum ClassCode {
 impl ClassCode {
     fn new(base_class: u8, sub_class: u8, programming_interface: u8) -> Self {
         match (base_class, sub_class, programming_interface) {
-            (0, 0, 0) => Self::AllCurrentlyImplementedDevice,
+            (0, 0, 0) => Self::AllCurrentlyImplemented,
             (0, 1, 0) => Self::VGACompatibleDevice,
             (base_class, sub_class, programming_interface) => Self::Other {
                 base_class,
