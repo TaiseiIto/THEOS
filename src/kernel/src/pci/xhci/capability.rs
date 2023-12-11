1b8316ca616d746f5276e4d77ab13cf5d9c8f902
TaiseiIto <taisei1212@outlook.jp>
2023-12-11 22:26:09 +0900

    Read xHCI registers #29

--- src/kernel/src/pci/xhci/capability.rs
+++ src/kernel/src/pci/xhci/capability.rs
@@ -32,7 +32,7 @@ impl Hcsparams1 {
     const MAX_INTERRUPTERS_END: usize = Self::MAX_INTERRUPTERS_BEGIN + Self::MAX_INTERRUPTERS_LENGTH;
     const MAX_INTERRUPTERS_MASK: u32 = (1 << Self::MAX_INTERRUPTERS_END) - (1 << Self::MAX_INTERRUPTERS_BEGIN);
     const RESERVED_BEGIN: usize = Self::MAX_INTERRUPTERS_END;
-    const RESERVED_LENGTH: usize = 4;
+    const RESERVED_LENGTH: usize = 5;
     const RESERVED_END: usize = Self::RESERVED_BEGIN + Self::RESERVED_LENGTH;
     const MAX_PORTS_BEGIN: usize = Self::RESERVED_END;
     const MAX_PORTS_MASK: u32 = u32::MAX - (1 << Self::MAX_PORTS_BEGIN) + 1;
@@ -75,6 +75,21 @@ impl Hcsparams2 {
     const EVENT_RING_SEGMENT_TABLE_MAX_LENGTH: usize = 4;
     const EVENT_RING_SEGMENT_TABLE_MAX_END: usize = Self::EVENT_RING_SEGMENT_TABLE_MAX_BEGIN + Self::EVENT_RING_SEGMENT_TABLE_MAX_LENGTH;
     const EVENT_RING_SEGMENT_TABLE_MAX_MASK: u32 = (1 << Self::EVENT_RING_SEGMENT_TABLE_MAX_END) - (1 << Self::EVENT_RING_SEGMENT_TABLE_MAX_BEGIN);
+    const RESERVED_BEGIN: usize = Self::EVENT_RING_SEGMENT_TABLE_MAX_END;
+    const RESERVED_LENGTH: usize = 13;
+    const RESERVED_END: usize = Self::RESERVED_BEGIN + Self::RESERVED_LENGTH;
+    const MAX_SCRATCHPAD_BUFFERS_HIGH_BEGIN: usize = Self::RESERVED_END;
+    const MAX_SCRATCHPAD_BUFFERS_HIGH_LENGTH: usize = 5;
+    const MAX_SCRATCHPAD_BUFFERS_HIGH_END: usize = Self::MAX_SCRATCHPAD_BUFFERS_HIGH_BEGIN + Self::MAX_SCRATCHPAD_BUFFERS_HIGH_LENGTH;
+    const MAX_SCRATCHPAD_BUFFERS_HIGH_MASK: u32 = (1 << Self::MAX_SCRATCHPAD_BUFFERS_HIGH_END) - (1 << Self::MAX_SCRATCHPAD_BUFFERS_HIGH_BEGIN);
+    const SCRATCHPAD_RESTORE_BEGIN: usize = Self::MAX_SCRATCHPAD_BUFFERS_HIGH_END;
+    const SCRATCHPAD_RESTORE_LENGTH: usize = 1;
+    const SCRATCHPAD_RESTORE_END: usize = Self::SCRATCHPAD_RESTORE_BEGIN + Self::SCRATCHPAD_RESTORE_LENGTH;
+    const SCRATCHPAD_RESTORE_MASK: u32 = (1 << Self::SCRATCHPAD_RESTORE_END) - (1 << Self::SCRATCHPAD_RESTORE_BEGIN);
+    const MAX_SCRATCHPAD_BUFFERS_LOW_BEGIN: usize = Self::SCRATCHPAD_RESTORE_END;
+    const MAX_SCRATCHPAD_BUFFERS_LOW_LENGTH: usize = 5;
+    const MAX_SCRATCHPAD_BUFFERS_LOW_END: usize = Self::MAX_SCRATCHPAD_BUFFERS_LOW_BEGIN + Self::MAX_SCRATCHPAD_BUFFERS_LOW_LENGTH;
+    const MAX_SCRATCHPAD_BUFFERS_LOW_MASK: u32 = u32::MAX - (1 << Self::MAX_SCRATCHPAD_BUFFERS_LOW_BEGIN) + 1;
 
     fn isochronous_scheduling_threshold(&self) -> u8 {
         ((self.0 & Self::ISOCHRONOUS_SCHEDULING_THRESHOLD_MASK) >> Self::ISOCHRONOUS_SCHEDULING_THRESHOLD_BEGIN) as u8
@@ -82,6 +97,22 @@ impl Hcsparams2 {
 
     fn event_ring_segment_table_max(&self) -> u8 {
         ((self.0 & Self::EVENT_RING_SEGMENT_TABLE_MAX_MASK) >> Self::EVENT_RING_SEGMENT_TABLE_MAX_BEGIN) as u8
+    }
+
+    fn max_scratchpad_buffers(&self) -> u16 {
+        ((self.max_scratchpad_buffers_high() as u16) << Self::MAX_SCRATCHPAD_BUFFERS_LOW_LENGTH) + (self.max_scratchpad_buffers_low() as u16)
+    }
+
+    fn max_scratchpad_buffers_low(&self) -> u8 {
+        ((self.0 & Self::MAX_SCRATCHPAD_BUFFERS_LOW_MASK) >> Self::MAX_SCRATCHPAD_BUFFERS_LOW_BEGIN) as u8
+    }
+
+    fn max_scratchpad_buffers_high(&self) -> u8 {
+        ((self.0 & Self::MAX_SCRATCHPAD_BUFFERS_HIGH_MASK) >> Self::MAX_SCRATCHPAD_BUFFERS_HIGH_BEGIN) as u8
+    }
+
+    fn scratchpad_restore(&self) -> bool {
+        self.0 & Self::SCRATCHPAD_RESTORE_MASK != 0
     }
 }
 
@@ -91,6 +122,8 @@ impl fmt::Debug for Hcsparams2 {
             .debug_struct("HCSPARAMS2")
             .field("isochronous_scheduling_threshold", &self.isochronous_scheduling_threshold())
             .field("event_ring_segment_table_max", &self.event_ring_segment_table_max())
+            .field("max_scratchpad_buffers", &self.max_scratchpad_buffers())
+            .field("scratchpad_restore", &self.scratchpad_restore())
             .finish()
     }
 }
