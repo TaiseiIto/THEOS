559ef6932731503da9cc4eb6ac3600c5981da803
TaiseiIto <taisei1212@outlook.jp>
2023-12-11 21:31:32 +0900

    Read xHCI registers #29

--- src/kernel/src/pci/xhci/capability.rs
+++ src/kernel/src/pci/xhci/capability.rs
@@ -1,3 +1,5 @@
+use core::fmt;
+
 // https://www.intel.com/content/dam/www/public/us/en/documents/technical-specifications/extensible-host-controler-interface-usb-xhci.pdf
 // 5.3 Host Controller Capability Registers
 #[derive(Debug)]
@@ -6,7 +8,7 @@ pub struct Registers {
     caplength: u8,
     rsvd: u8,
     hciversion: u16,
-    hcsparams1: u32,
+    hcsparams1: Hcsparams1,
     hcsparams2: u32,
     hcsparams3: u32,
     hccparams1: u32,
@@ -14,3 +16,29 @@ pub struct Registers {
     rtsoff: u32,
     hccparams2: u32,
 }
+
+// https://www.intel.com/content/dam/www/public/us/en/documents/technical-specifications/extensible-host-controler-interface-usb-xhci.pdf
+// 5.3.3 Structural Parameters 1 (HCSPARAMS1)
+#[derive(Clone, Copy)]
+struct Hcsparams1(u32);
+
+impl Hcsparams1 {
+    const MAX_DEVICE_SLOTS_BEGIN: usize = 0;
+    const MAX_DEVICE_SLOTS_LENGTH: usize = 8;
+    const MAX_DEVICE_SLOTS_END: usize = Self::MAX_DEVICE_SLOTS_BEGIN + Self::MAX_DEVICE_SLOTS_LENGTH;
+    const MAX_DEVICE_SLOTS_MASK: u32 = (1 << Self::MAX_DEVICE_SLOTS_END) - (1 << Self::MAX_DEVICE_SLOTS_BEGIN);
+
+    fn max_device_slots(&self) -> u8 {
+        (self.0 & Self::MAX_DEVICE_SLOTS_MASK) as u8
+    }
+}
+
+impl fmt::Debug for Hcsparams1 {
+    fn fmt(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
+        formatter
+            .debug_struct("HCSPARAMS1")
+            .field("max_device_slots", &self.max_device_slots())
+            .finish()
+    }
+}
+
