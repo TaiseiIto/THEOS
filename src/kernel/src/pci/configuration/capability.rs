152934949cd40b5ba53774e4d5f165c3a7df696c
TaiseiIto <taisei1212@outlook.jp>
2023-12-18 13:17:03 +0900

    Iterate PCI capability structures #29

--- src/kernel/src/pci/configuration/capability.rs
+++ src/kernel/src/pci/configuration/capability.rs
@@ -5,7 +5,7 @@ use {
     super::CONFIGURATION_SIZE,
 };
 
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 pub struct Structure {
     capability_id: u8,
     next_capability_pointer: u8,
@@ -32,6 +32,7 @@ impl Structure {
     }
 }
 
+#[derive(Clone)]
 struct StructureIterator<'a> {
     configuration: &'a [u8; CONFIGURATION_SIZE],
     structure: Option<Structure>,
@@ -47,11 +48,19 @@ impl<'a> StructureIterator<'a> {
     }
 }
 
-impl Iterator for StructureIterator<'_> {
+impl<'a> Iterator for StructureIterator<'a> {
     type Item = Structure;
 
     fn next(&mut self) -> Option<Self::Item> {
-        None
+        let StructureIterator {
+            configuration,
+            structure,
+        } = self.clone();
+        if let Some(structure) = &structure {
+            let next_capability_pointer: u8 = structure.next_capability_pointer;
+            self.structure = Structure::get_one(configuration, next_capability_pointer);
+        }
+        structure
     }
 }
 
